4b62489ffae8c4fb9d2a5023e71abcbf0acc5fa4
dreamerminsk <David70908019Miller>
2020-02-02 21:53:54 +0300

    random page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:collection';
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:provider/provider.dart';
@@ -16,7 +17,12 @@ class News with ChangeNotifier {
   final List<String> _titles = [];
 
   News() {
-    addNews();
+    var rng = new Random();
+    if (rng.nextInt(100) > 50) {
+      addWeekBoxOffice();
+    } else {
+      addYearBoxOffice();
+    }
   }
 
   getLoading() => _loading;
@@ -29,14 +35,14 @@ class News with ChangeNotifier {
 
   UnmodifiableListView<String> get titles => UnmodifiableListView(_titles);
 
-  void addNews() async {
+  void addYearBoxOffice() async {
     _loading = true;
     _news = "...";
     notifyListeners();
     try {
       Dio dio = new Dio();
       var response =
-          await dio.get('http://kinobusiness.com/kassovye_sbory/films_year/');
+      await dio.get('http://kinobusiness.com/kassovye_sbory/films_year/');
       var document = parse(response.data.toString());
       List<dom.Element> links = document.querySelectorAll('title');
       _loading = false;
@@ -47,7 +53,7 @@ class News with ChangeNotifier {
       _titles.clear();
       _titles.addAll(ms.map((e) => e.text).toList());
       List<dom.Element> rows =
-          document.querySelectorAll('table#krestable > tbody  > tr');
+      document.querySelectorAll('table#krestable > tbody  > tr');
       rows.map(toRec);
       _titles.clear();
       _titles.addAll(rows.map(toRec).toList());
@@ -58,11 +64,48 @@ class News with ChangeNotifier {
     }
   }
 
+  void addWeekBoxOffice() async {
+    _loading = true;
+    _news = "...";
+    notifyListeners();
+    try {
+      Dio dio = new Dio();
+      var response =
+      await dio.get(
+          'http://kinobusiness.com/kassovye_sbory/weekend/2020/26.01.2020/');
+      var document = parse(response.data.toString());
+      List<dom.Element> links = document.querySelectorAll('title');
+      _loading = false;
+      _news = links[0].text;
+      notifyListeners();
+      List<dom.Element> ms = document
+          .querySelectorAll('table#krestable > tbody  > tr > td > b > a');
+      _titles.clear();
+      _titles.addAll(ms.map((e) => e.text).toList());
+      List<dom.Element> rows =
+      document.querySelectorAll('table#krestable > tbody  > tr');
+      rows.map(toRec);
+      _titles.clear();
+      _titles.addAll(rows.map(toRec2).toList());
+    } catch (exception) {
+    _loading = false;
+    _news = "ERROR - " + exception.runtimeType.toString();
+    notifyListeners();
+    }
+  }
+
   String toRec(dom.Element e) {
     var children = e.getElementsByTagName('td');
     //var t = '';
     //children.forEach((c) => {t = t + c.text});
     return children[1].text + '\r\n' + children[5].text;
+  }
+
+  String toRec2(dom.Element e) {
+    var children = e.getElementsByTagName('td');
+    //var t = '';
+    //children.forEach((c) => {t = t + c.text});
+    return children[3].text + '\r\n' + children[6].text;
   }
 }
 
@@ -82,6 +125,7 @@ class MyApp extends StatelessWidget {
 }
 
 class NewsPage extends StatelessWidget {
+  final rng = new Random();
   @override
   Widget build(BuildContext context) {
     final news = Provider.of<News>(context);
@@ -91,57 +135,61 @@ class NewsPage extends StatelessWidget {
         IconButton(
           icon: Icon(Icons.autorenew),
           onPressed: () async {
-            news.addNews();
+            if (rng.nextInt(100) > 50) {
+              news.addWeekBoxOffice();
+            } else {
+              news.addYearBoxOffice();
+            }
           },
         ),
       ]),
       body: news.getLoading()
           ? Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  LoadingIndicator(
-                    indicatorType: Indicator.ballClipRotateMultiple,
-                  ),
-                ],
-              ),
-            )
-          : ListView.separated(
-              shrinkWrap: true,
-              padding: const EdgeInsets.all(8),
-              itemCount: news.titles.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  //color: Colors.indigo,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      SizedBox(
-                        width: 8,
-                      ),
-                      Text('${index + 1}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 28)),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Flexible(
-                        child: Text('${news.titles[index]}',
-                            style: TextStyle(
-                                //color: Colors.white,
-                                fontWeight: FontWeight.normal,
-                                fontSize: 19)),
-                      ),
-                    ],
-                  ),
-                );
-              },
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            LoadingIndicator(
+              indicatorType: Indicator.ballClipRotateMultiple,
             ),
+          ],
+        ),
+      )
+          : ListView.separated(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(8),
+        itemCount: news.titles.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            //color: Colors.indigo,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                SizedBox(
+                  width: 8,
+                ),
+                Text('${index + 1}',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 30)),
+                SizedBox(
+                  width: 12,
+                ),
+                Flexible(
+                  child: Text('${news.titles[index]}',
+                      style: TextStyle(
+                        //color: Colors.white,
+                          fontWeight: FontWeight.normal,
+                          fontSize: 19)),
+                ),
+              ],
+            ),
+          );
+        },
+        separatorBuilder: (BuildContext context, int index) =>
+        const Divider(),
+      ),
       bottomNavigationBar: new BottomNavigationBar(
-        items:<BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem>[
           new BottomNavigationBarItem(
               icon: const Icon(Icons.home),
               title: new Text("Home")
