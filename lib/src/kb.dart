785cb314be13c5c139121c234813fd93eae7355d
dKCh <tutmirage@gmail.com>
2020-02-04 00:27:16 +0300

    Update kb.dart

--- lib/src/kb.dart
+++ lib/src/kb.dart
@@ -13,6 +13,22 @@ class YearRecord {
   YearRecord(this.pos, this.title, this.boxOffice);
 }
 
+class WeekendRecord {
+  int pos;
+  String title;
+  int boxOffice;
+
+  WeekendRecord(this.pos, this.title, this.boxOffice);
+}
+
+class ThursdayRecord {
+  int pos;
+  String title;
+  int boxOffice;
+
+  ThursdayRecord(this.pos, this.title, this.boxOffice);
+}
+
 class KbApi {
   static final yearBoxOffice =
       'http://kinobusiness.com/kassovye_sbory/films_year/';
@@ -39,5 +55,51 @@ class KbApi {
         int.parse(children[0].text.trim()),
         children[1].text.trim(),
         int.tryParse(children[5].text.trim().replaceAll((' '), '')) ?? 0);
+  }
+
+Future<List<WeekendRecord>> getWeekendBoxOffice() async {
+    try {
+      //return <YearRecord>[YearRecord(1, 'TEST', 1)];
+      var response = await dio.get(yearBoxOffice);
+      var document = parse(response.data.toString());
+      List<dom.Element> rows =
+      document.querySelectorAll('table#krestable > tbody  > tr');
+      developer.log('ELEMENTS: ${rows.length}');
+      return rows.map(toRec2).toList();
+    } catch (exception) {
+      developer.log(exception.toString());
+      return <WeekendRecord>[];
+    }
+  }
+
+  WeekendRecord toRec2(dom.Element e) {
+    var children = e.getElementsByTagName('td');
+    return WeekendRecord(
+        int.parse(children[0].text.trim()),
+        children[3].text.trim(),
+        int.tryParse(children[6].text.trim().replaceAll((' '), '')) ?? 0);
+  }
+
+Future<List<ThursdayRecord>> getThursdayBoxOffice() async {
+    try {
+      //return <YearRecord>[YearRecord(1, 'TEST', 1)];
+      var response = await dio.get(yearBoxOffice);
+      var document = parse(response.data.toString());
+      List<dom.Element> rows =
+      document.querySelectorAll('table#krestable > tbody  > tr');
+      developer.log('ELEMENTS: ${rows.length}');
+      return rows.map(toRec3).toList();
+    } catch (exception) {
+      developer.log(exception.toString());
+      return <ThursdayRecord>[];
+    }
+  }
+
+  ThursdayRecord toRec3(dom.Element e) {
+    var children = e.getElementsByTagName('td');
+    return ThursdayRecord(
+        int.parse(children[0].text.trim()),
+        children[3].text.trim(),
+        int.tryParse(children[6].text.trim().replaceAll((' '), '')) ?? 0);
   }
 }
