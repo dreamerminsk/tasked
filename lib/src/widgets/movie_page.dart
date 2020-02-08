7dea8372d9aba05ac2d7335ca61bf55d209a3f61
dKCh <tutmirage@gmail.com>
2020-02-09 01:33:28 +0300

    update movie_page widget

--- lib/src/widgets/movie_page.dart
+++ lib/src/widgets/movie_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:kbapp/src/kb/model.dart';
 
+import '../formatters.dart';
+
 class MovieModel with ChangeNotifier {}
 
 class MoviePage extends StatelessWidget {
@@ -12,14 +14,46 @@ class MoviePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('${this.movie.title}'),
+          title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${this.movie.title}',
+                style: Theme
+                    .of(context)
+                    .primaryTextTheme
+                    .title,
+              ),
+              Text(
+                '${this.movie.original}',
+                style: Theme
+                    .of(context)
+                    .primaryTextTheme
+                    .subtitle,
+              )
+            ],
+          ),
+          actions: <Widget>[new Icon(Icons.more_vert)],
         ),
         body: Center(
           child: Column(children: <Widget>[
-            Text('${this.movie.title}'),
-            Text('${this.movie.original}'),
-            Text('${this.movie.distributor}'),
-            Text('${this.movie.boxOffice} / ${this.movie.spectaculars}'),
+            Text(
+              '${this.movie.distributor}',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .display1,
+            ),
+            Text(
+              '${decimalFormatter.format(
+                  this.movie.boxOffice)} / ${decimalFormatter.format(
+                  this.movie.spectaculars)}',
+              style: Theme
+                  .of(context)
+                  .textTheme
+                  .title,
+            ),
           ]),
         ));
   }
