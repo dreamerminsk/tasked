64903025336b7594ad573e1f60a9565e92b7ba2b
dKCh <tutmirage@gmail.com>
2020-02-09 11:56:38 +0300

    Update strings.dart

--- lib/src/utils/strings.dart
+++ lib/src/utils/strings.dart
@@ -1,1 +1,8 @@
 
+import 'package:intl/intl.dart';
+
+final fullDateFormatter = new DateFormat('dd.MM.yyyy');
+
+final yearFormatter = new DateFormat('yyyy');
+
+final decimalFormatter = new NumberFormat("#,##0", "en_US");
