24463f078026f40b0f1c7968e36d95b7fd294dfc
Micah Teeuws <tutmirage@gmail.com>
2022-02-19 11:37:06 +0300

    Create kinobussiness_test.dart

---
+++ test/kinobussiness_test.dart
@@ -0,0 +1,8 @@
+import 'package:test/test.dart';
+
+void main() {
+  test('MyWidget has a title and message', ()  {
+    //expect(titleFinder, findsOneWidget);
+    //expect(messageFinder, findsOneWidget);
+  });
+}
