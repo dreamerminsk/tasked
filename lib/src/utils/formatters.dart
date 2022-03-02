4299d93e6c0444efbfbf7eedfbc70f5f534fcbba
Micah Teeuws <tutmirage@gmail.com>
2022-03-02 18:07:57 +0300

    Delete formatters.dart

--- lib/src/utils/formatters.dart
+++
@@ -1,7 +0,0 @@
-import 'package:intl/intl.dart';
-
-final fullDateFormatter = new DateFormat('dd.MM.yyyy');
-
-final yearFormatter = new DateFormat('yyyy');
-
-final decimalFormatter = new NumberFormat("#,##0", "en_US");
