575a10d0d85ce97b046c90d5a1ccd028991ca7fa
Micah Teeuws <tutmirage@gmail.com>
2022-03-02 16:30:40 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:kb_dart/src/widgets/year_box_office_page.dart';
 import 'package:provider/provider.dart';
 
+import 'package:get/get.dart';
+
 import 'src/widgets/box_office_page.dart';
 import 'src/widgets/thursday_box_office_page.dart';
 import 'src/widgets/weekend_box_office_page.dart';
