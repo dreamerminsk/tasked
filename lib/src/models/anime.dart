267d98614195a8b682db73a948750ca47020961a
Micah Teeuws <tutmirage@gmail.com>
2022-03-03 11:58:34 +0300

    Update anime.dart

--- lib/src/models/anime.dart
+++ lib/src/models/anime.dart
@@ -1,3 +1,23 @@
 class Anime {
 
 }
+
+Class
+class DemoModel {
+  int num1;
+  int num2;
+
+  DemoModel({this.num1, this.num2});
+
+  DemoModel.fromJson(Map<String, dynamic> json) {
+    num1 = json['num1'];
+    num2 = json['num2'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['num1'] = this.num1;
+    data['num2'] = this.num2;
+    return data;
+  }
+}
