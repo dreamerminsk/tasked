1680a759b2ae195b95686760c076e1ea26282a63
Micah Teeuws <tutmirage@gmail.com>
2022-03-03 09:31:55 +0300

    Update home_controller.dart

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -5,7 +5,7 @@ import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
-  static final anime_ref = 'https://raw.githubusercontent.com/dreamerminsk/kb-dart/first-version/data/wiki.anime.txt';
+  static final animeRef = 'https://raw.githubusercontent.com/dreamerminsk/kb-dart/first-version/data/wiki.anime.txt';
   final animeList = [].obs;
   var count = 0.obs;
   increment() => count++;
