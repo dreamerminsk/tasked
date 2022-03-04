3a1c11624caab289051d08975c222d42556e77d5
Micah Teeuws <tutmirage@gmail.com>
2022-03-04 16:46:52 +0300

    Create debug_view.dart

---
+++ lib/src/views/debug_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class DebugView extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    final HomeController c = Get.get(HomeController());
+
+    return Scaffold(
+      appBar: AppBar(title: Obx(() => Text("Anime: ${c.animeList.length}")),
+              actions: <Widget>[IconButton(icon: Icon(Icons.copy), onPressed: (){},)]),
+
+      body: Obx( () =>
+      const Center(child: Text('No items'))),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.refresh), onPressed: c.refresh));
+  }
+}
