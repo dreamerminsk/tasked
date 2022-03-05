38fcb2656d4c3d8f70380c36485897ffffb19e12
Micah Teeuws <tutmirage@gmail.com>
2022-03-05 15:24:54 +0300

    Update anime.dart

--- lib/src/models/anime.dart
+++ lib/src/models/anime.dart
@@ -1,45 +1,22 @@
+import 'dart:convert';
+
 import 'wiki.dart';
 
 class Anime {
   String? title;
-  String? wikiTitle;
-  String? wikiImage;
-  int mviMonth = 0;
+  Wiki? wiki;
 
-  Anime({this.title, this.wikiTitle, this.wikiImage, this.mviMonth = 0});
+  Anime({this.title, this.wiki});
 
   Anime.fromJson(Map<String, dynamic> json) {
     title = json['title'];
-    wikiTitle = json['wikiTitle'];
-    wikiImage = json['wikiImage'];
-    mviMonth = json['mviMonth'] ?? 0;
+    wiki = Wiki.fromJson(json['wiki']);
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['title'] = this.title;
-    data['wikiTitle'] = this.wikiTitle;
-    data['wikiImage'] = this.wikiImage;
-    data['mviMonth'] = this.mviMonth;
-    return data;
-  }
-}
-
-
-
-class AnimeModel {
-  String? title;
-  Wiki? wiki;
-
-  AnimeModel({this.title, this.wiki});
-
-  AnimeModel.fromJson(Map<String, dynamic> json) {
-    title = json['title'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['title'] = this.title;
+    data['wiki'] = this.wiki != null ? this.wiki.toJson() : null;
     return data;
   }
 }
