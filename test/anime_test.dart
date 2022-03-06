3718377b07d8930d2ecf52ad5d5bffaf48d39c1c
Micah Teeuws <tutmirage@gmail.com>
2022-03-06 07:37:08 +0300

    Create anime_test.dart

---
+++ test/anime_test.dart
@@ -0,0 +1,28 @@
+import 'dart:convert';
+import 'dart:io';
+
+//import 'package:html/dom.dart' as dom;
+//import 'package:html/parser.dart';
+
+import 'package:test/test.dart';
+
+void main() {
+  final file = new File('data/wiki.anime.json');
+  
+  test('wiki.anime.json is exists', ()  { 
+    expect(file.existsSync(), true);
+  });
+  
+  final jsonString = file.readAsStringSync();
+  
+  test('wiki.anime.json is readable', ()  { 
+    expect(jsonString.length, greaterThan(0));
+  });
+  
+  final jsonList = jsonDecode(jsonString);
+  
+  test('wiki.anime.json is parceable', ()  {    
+    expect(jsonList.length, 89);
+  });
+  
+}
