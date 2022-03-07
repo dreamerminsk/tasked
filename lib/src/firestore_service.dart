126ad9b7db99765305a7fe9446279cabaefe47ae
Micah Teeuws <tutmirage@gmail.com>
2022-03-07 18:57:46 +0300

    Update firestore_service.dart

--- lib/src/firestore_service.dart
+++ lib/src/firestore_service.dart
@@ -1,4 +1,4 @@
-import 'package:firestore_core/firestore_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'models/anime.dart';
 //import 'package:kbapp/src/kb/model.dart';
