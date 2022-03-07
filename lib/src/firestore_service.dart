8b29b60a83ef822ada941adb4fdf3db6d848ecc1
Micah Teeuws <tutmirage@gmail.com>
2022-03-07 18:25:45 +0300

    Update firestore_service.dart

--- lib/src/firestore_service.dart
+++ lib/src/firestore_service.dart
@@ -1,21 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:kbapp/src/kb/model.dart';
-import 'package:kbapp/src/utils/formatters.dart';
+//import 'package:kbapp/src/kb/model.dart';
+//import 'package:kbapp/src/utils/formatters.dart';
 
-final CollectionReference yearCollection =
-Firestore.instance.collection('boxoffice_years');
-
-final CollectionReference thursdayCollection =
-Firestore.instance.collection('thursdays');
-
-final CollectionReference weekendCollection =
-Firestore.instance.collection('weekends');
-
-final CollectionReference workerCollection =
-Firestore.instance.collection('workers');
-
-final CollectionReference personCollection =
-Firestore.instance.collection('persons');
+final CollectionReference animeCollection =
+Firestore.instance.collection('anime');
 
 class FirestoreService {
   static final FirestoreService _instance = new FirestoreService.internal();
@@ -23,109 +11,6 @@ class FirestoreService {
   factory FirestoreService() => _instance;
 
   FirestoreService.internal();
-
-  Future<Thursday> createThursday(Thursday thursday) async {
-    final TransactionHandler createTransaction = (Transaction tx) async {
-      final DocumentSnapshot ds = await tx
-          .get(thursdayCollection.document(isoFormatter.format(thursday.date)));
-
-      final Map<String, dynamic> data = thursday.toMap();
-
-      await tx.set(ds.reference, data);
-
-      return data;
-    };
-
-    return Firestore.instance.runTransaction(createTransaction).then((mapData) {
-      return Thursday.fromMap(mapData);
-    }).catchError((error) {
-      print('error: $error');
-      return null;
-    });
-  }
-
-  Stream<QuerySnapshot> getThursdayList({int offset, int limit}) {
-    Stream<QuerySnapshot> snapshots =
-    thursdayCollection.orderBy('date', descending: true).snapshots();
-
-    if (offset != null) {
-      snapshots = snapshots.skip(offset);
-    }
-
-    if (limit != null) {
-      snapshots = snapshots.take(limit);
-    }
-
-    return snapshots;
-  }
-
-  Future<Weekend> createWeekend(Weekend weekend) async {
-    final TransactionHandler createTransaction = (Transaction tx) async {
-      final DocumentSnapshot ds = await tx
-          .get(weekendCollection.document(isoFormatter.format(weekend.date)));
-
-      final Map<String, dynamic> data = weekend.toMap();
-
-      await tx.set(ds.reference, data);
-
-      return data;
-    };
-
-    return Firestore.instance.runTransaction(createTransaction).then((mapData) {
-      return Weekend.fromMap(mapData);
-    }).catchError((error) {
-      print('error: $error');
-      return null;
-    });
-  }
-
-  Stream<QuerySnapshot> getWeekendList({int offset, int limit}) {
-    Stream<QuerySnapshot> snapshots =
-    weekendCollection.orderBy('date', descending: true).snapshots();
-
-    if (offset != null) {
-      snapshots = snapshots.skip(offset);
-    }
-
-    if (limit != null) {
-      snapshots = snapshots.take(limit);
-    }
-
-    return snapshots;
-  }
-
-  Future<YearRecord> createYear(YearRecord year) async {
-    final TransactionHandler createTransaction = (Transaction tx) async {
-      final DocumentSnapshot ds = await tx.get(yearCollection.document());
-
-      final Map<String, dynamic> data = year.toMap();
-
-      await tx.set(ds.reference, data);
-
-      return data;
-    };
-
-    return Firestore.instance.runTransaction(createTransaction).then((mapData) {
-      return YearRecord.fromMap(mapData);
-    }).catchError((error) {
-      print('error: $error');
-      return null;
-    });
-  }
-
-  Stream<QuerySnapshot> getYearList({int offset, int limit}) {
-    Stream<QuerySnapshot> snapshots = yearCollection.orderBy('pos').snapshots();
-
-    if (offset != null) {
-      snapshots = snapshots.skip(offset);
-    }
-
-    if (limit != null) {
-      snapshots = snapshots.take(limit);
-    }
-
-    return snapshots;
-  }
 
   Future<Person> createPerson(String fullName, String avatar) async {
     final TransactionHandler createTransaction = (Transaction tx) async {
