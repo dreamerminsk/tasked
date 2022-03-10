969cb1dfd4200544f5d82d83d3402283eafc9bd9
Micah Teeuws <tutmirage@gmail.com>
2022-03-10 11:16:18 +0300

    Create anime_editor.dart

---
+++ lib/src/views/anime_editor.dart
@@ -0,0 +1,42 @@
+import 'dart:convert';
+
+import 'package:flutter_json_viewer/flutter_json_viewer.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/home_controller.dart';
+
+class AnimeEditor extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    final HomeController c = Get.find();
+
+    return Scaffold(
+      appBar: AppBar(title: Obx(() => Text("Anime: ${c.animeList.length}")),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.copy),
+                  onPressed: () { 
+                    c.copyToClipboard();
+                  },
+                )
+              ]),
+
+      body: Obx( () {
+        final encoder = JsonEncoder.withIndent('   ');
+        return _scrollable(encoder.convert(c.animeList));
+      }),
+    );
+  }
+
+  Widget _scrollable(String text) {
+    return Row(children: <Widget>[
+             Expanded(
+               child: new SingleChildScrollView(
+                 scrollDirection: Axis.vertical,
+                   child: JsonViewer(jsonDecode(text)),
+               ),
+             ),]
+           );
+  }
+}
