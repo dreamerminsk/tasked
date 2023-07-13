4b60f347146fcb7baf8214c60d64fa0f565f077b
Micah Teeuws <tutmirage@gmail.com>
2023-07-13 13:22:05 +0300

    Create cats_view.dart

---
+++ lib/src/views/cats_view.dart
@@ -0,0 +1,55 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../controllers/cats_controller.dart';
+import 'debug_view.dart';
+import 'anime_details_view.dart';
+
+class CatsView extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    final CatsController c = Get.put(CatsController());
+
+    return Scaffold(
+      appBar: AppBar(title: Text("WikiStatsApp"),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.account_balance), onPressed: () { Get.to(DebugView()); },),
+                IconButton(icon: Icon(Icons.query_stats), onPressed: () { Get.to(DebugView()); },),
+              ]),
+
+      body: Obx( () =>
+      c.animeList.length > 0
+      ? ListView.builder(
+          itemCount: c.animeList.length,
+          itemBuilder: (BuildContext context, int index) {
+            return InkWell(onTap: () { c.select(index); Get.to(AnimeDetailsView());},
+             child: Card(
+              child: ListTile(
+                leading: c.animeList[index].wiki?.image != null
+                  ? CachedNetworkImage(
+                    imageUrl: (c.animeList[index].wiki?.image ?? '').replaceFirst('220px', '64px'),
+                    placeholder: (context, url) => CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => Icon(Icons.error),
+                    imageBuilder: (context, image) => Container(
+                      width: 64.00,
+                      height: 100.00,
+                      decoration: new BoxDecoration(
+                        image: new DecorationImage(
+                          image: image,
+                          fit: BoxFit.contain,
+                        ),
+                      ))
+                  )
+                  : FlutterLogo(size: 56.0),
+                title: Text('${c.animeList[index].title}'),
+                subtitle: Text('${c.animeList[index].wiki?.mviMonth ?? 0} - ${c.animeList[index].wiki?.lastUpdate.toString() ?? "..."}'),
+            ),));
+          },
+        )
+      : const Center(child: Text('No items'))),
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.refresh), onPressed: c.refresh));
+  }
+}
