da0567b570937b3ceed98aab3bb365a3a034a283
Micah Teeuws <tutmirage@gmail.com>
2023-07-13 13:19:10 +0300

    Update home_controller.dart

--- lib/src/controllers/home_controller.dart
+++ lib/src/controllers/home_controller.dart
@@ -8,32 +8,10 @@ import 'package:get/get.dart';
 import 'package:html/parser.dart';
 
 import '../models/anime.dart';
-import '../models/category.dart';
 
 class HomeController extends GetxController {
   static final animeRef = 'https://raw.githubusercontent.com/dreamerminsk/kb-dart/master/data/2023.anime.json';
   final animeList = <Anime>[].obs;
-  final categories = <Category>[
-    'Category:2023 films',
-    'Category:21st-century actresses',
-    'Category:21st-century actors',
-    'Category:2023 television series debuts',
-    'Category:2023 American television series debuts',
-    'Category:2023 television series endings',
-    'Category:2023 in animation',
-    'Category:2023 anime',
-    'Category:2023 anime television series debuts',
-    'Category:2023 books',
-    'Category:2023 novels',
-    'Category:2023 British novels',
-    'Category:Musical groups established in 2023',
-    'Category:Musical groups disestablished in 2023',
-    'Category:2023 albums',
-    'Category:2023 singles',
-    'Category:2023 songs',
-    'Category:2023 video games'
-  ].obs;
-  var year = 2023.obs;
   var selected = Anime().obs;
 
   @override
