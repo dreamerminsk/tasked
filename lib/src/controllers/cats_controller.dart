8acd2a7e824da79f47eb99ed1775211870a824b2
Micah Teeuws <tutmirage@gmail.com>
2023-07-14 12:44:46 +0300

    Update cats_controller.dart

--- lib/src/controllers/cats_controller.dart
+++ lib/src/controllers/cats_controller.dart
@@ -11,24 +11,24 @@ import '../models/category.dart';
 class CatsController extends GetxController {
   static final animeRef = 'https://raw.githubusercontent.com/dreamerminsk/kb-dart/master/data/2023.anime.json';
   final categories = <Category>[
-    'Category:2023 films',
-    'Category:21st-century actresses',
-    'Category:21st-century actors',
-    'Category:2023 television series debuts',
-    'Category:2023 American television series debuts',
-    'Category:2023 television series endings',
-    'Category:2023 in animation',
-    'Category:2023 anime',
-    'Category:2023 anime television series debuts',
-    'Category:2023 books',
-    'Category:2023 novels',
-    'Category:2023 British novels',
-    'Category:Musical groups established in 2023',
-    'Category:Musical groups disestablished in 2023',
-    'Category:2023 albums',
-    'Category:2023 singles',
-    'Category:2023 songs',
-    'Category:2023 video games'
+    Category('en', '2023 films'),
+    Category('en', '21st-century actresses'),
+    Category('en', '21st-century actors'),
+    Category('en', '2023 television series debuts'),
+    Category('en', '2023 American television series debuts'),
+    Category('en', '2023 television series endings'),
+    Category('en', '2023 in animation'),
+    Category('en', '2023 anime'),
+    Category('en', '2023 anime television series debuts'),
+    Category('en', '2023 books'),
+    Category('en', '2023 novels'),
+    Category('en', '2023 British novels'),
+    Category('en', 'Musical groups established in 2023'),
+    Category('en', 'Musical groups disestablished in 2023'),
+    Category('en', '2023 albums'),
+    Category('en', '2023 singles'),
+    Category('en', '2023 songs'),
+    Category('en', '2023 video games')
   ].obs;
   var year = 2023.obs;
 
