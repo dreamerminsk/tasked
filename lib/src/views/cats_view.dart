7067fa663c939ae32905747b28fe0e9a40ac7794
Micah Teeuws <tutmirage@gmail.com>
2023-07-14 10:09:21 +0300

    Update cats_view.dart

--- lib/src/views/cats_view.dart
+++ lib/src/views/cats_view.dart
@@ -5,6 +5,7 @@ import 'package:get/get.dart';
 import '../controllers/cats_controller.dart';
 import 'debug_view.dart';
 import 'anime_details_view.dart';
+import 'home_view.dart';
 
 class CatsView extends StatelessWidget {
 
@@ -15,36 +16,22 @@ class CatsView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text("WikiStatsApp"),
               actions: <Widget>[
+                IconButton(icon: Icon(Icons.home), onPressed: () { Get.to(HomeView()); },),
                 IconButton(icon: Icon(Icons.account_balance), onPressed: () { Get.to(DebugView()); },),
                 IconButton(icon: Icon(Icons.query_stats), onPressed: () { Get.to(DebugView()); },),
               ]),
 
       body: Obx( () =>
-      c.animeList.length > 0
+      c.categories.length > 0
       ? ListView.builder(
-          itemCount: c.animeList.length,
+          itemCount: c.categories.length,
           itemBuilder: (BuildContext context, int index) {
-            return InkWell(onTap: () { c.select(index); Get.to(AnimeDetailsView());},
+            return InkWell(onTap: () { Get.to(AnimeDetailsView());},
              child: Card(
               child: ListTile(
-                leading: c.animeList[index].wiki?.image != null
-                  ? CachedNetworkImage(
-                    imageUrl: (c.animeList[index].wiki?.image ?? '').replaceFirst('220px', '64px'),
-                    placeholder: (context, url) => CircularProgressIndicator(),
-                    errorWidget: (context, url, error) => Icon(Icons.error),
-                    imageBuilder: (context, image) => Container(
-                      width: 64.00,
-                      height: 100.00,
-                      decoration: new BoxDecoration(
-                        image: new DecorationImage(
-                          image: image,
-                          fit: BoxFit.contain,
-                        ),
-                      ))
-                  )
-                  : FlutterLogo(size: 56.0),
-                title: Text('${c.animeList[index].title}'),
-                subtitle: Text('${c.animeList[index].wiki?.mviMonth ?? 0} - ${c.animeList[index].wiki?.lastUpdate.toString() ?? "..."}'),
+                leading: FlutterLogo(size: 64.0),
+                title: Text('${c.categories[index].title}'),
+                subtitle: Text('...'),
             ),));
           },
         )
