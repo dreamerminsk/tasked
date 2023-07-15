8d9632b06520a1b3100151ba295307501fda63b3
Micah Teeuws <tutmirage@gmail.com>
2023-07-15 11:22:35 +0300

    Update home_view.dart

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
 import '../controllers/home_controller.dart';
 import 'debug_view.dart';
