a9f13f1f8edeab1ca38d9d78b56251d7ada37584
Micah Teeuws <tutmirage@gmail.com>
2023-07-16 21:51:49 +0300

    Update cats_controller.dart

--- lib/src/controllers/cats_controller.dart
+++ lib/src/controllers/cats_controller.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-import 'dart:convert';
 import 'dart:core';
 
 import 'package:dio/dio.dart';
@@ -11,24 +9,24 @@ import '../models/category.dart';
 class CatsController extends GetxController {
   static final animeRef = 'https://raw.githubusercontent.com/dreamerminsk/kb-dart/master/data/2023.anime.json';
   final categories = <Category>[
-    Category('en', '2023 films'),
-    Category('en', '21st-century actresses'),
-    Category('en', '21st-century actors'),
-    Category('en', '2023 television series debuts'),
-    Category('en', '2023 American television series debuts'),
-    Category('en', '2023 television series endings'),
-    Category('en', '2023 in animation'),
-    Category('en', '2023 anime'),
-    Category('en', '2023 anime television series debuts'),
-    Category('en', '2023 books'),
-    Category('en', '2023 novels'),
-    Category('en', '2023 British novels'),
-    Category('en', 'Musical groups established in 2023'),
-    Category('en', 'Musical groups disestablished in 2023'),
-    Category('en', '2023 albums'),
-    Category('en', '2023 singles'),
-    Category('en', '2023 songs'),
-    Category('en', '2023 video games')
+    Category(lang: 'en', title: '2023 films'),
+    Category(lang: 'en', title: '21st-century actresses'),
+    Category(lang: 'en', title: '21st-century actors'),
+    Category(lang: 'en', title: '2023 television series debuts'),
+    Category(lang: 'en', title: '2023 American television series debuts'),
+    Category(lang: 'en', title: '2023 television series endings'),
+    Category(lang: 'en', title: '2023 in animation'),
+    Category(lang: 'en', title: '2023 anime'),
+    Category(lang: 'en', title: '2023 anime television series debuts'),
+    Category(lang: 'en', title: '2023 books'),
+    Category(lang: 'en', title: '2023 novels'),
+    Category(lang: 'en', title: '2023 British novels'),
+    Category(lang: 'en', title: 'Musical groups established in 2023'),
+    Category(lang: 'en', title: 'Musical groups disestablished in 2023'),
+    Category(lang: 'en', title: '2023 albums'),
+    Category(lang: 'en', title: '2023 singles'),
+    Category(lang: 'en', title: '2023 songs'),
+    Category(lang: 'en', title: '2023 video games')
   ].obs;
   var year = 2023.obs;
 
