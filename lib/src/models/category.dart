ae69c22a0f5fb885570b92ad0c6c6e6490bc05e6
Micah Teeuws <tutmirage@gmail.com>
2023-11-13 18:51:47 +0300

    Update category.dart

--- lib/src/models/category.dart
+++ lib/src/models/category.dart
@@ -1,21 +1,23 @@
 class Category {
-  String? lang;
-  String? title;
+  int pageid;
+  String lang;
+  String title;
   int pages;
   int files;
   int subcats;
 
-  Category({this.lang, this.title, this.pages = 0, this.files = 0, this.subcats = 0});
+  Category({this.pageid = 0, this.lang = 'en', this.title = '', this.pages = 0, this.files = 0, this.subcats = 0});
 
   Category.fromJson(Map<String, dynamic> json) {
+    pageid = json['pageid'] ?? -1;
     lang = json['lang'] ?? 'en';
     title = json['title'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['lang'] = this.lang ?? 'en';
-    data['title'] = this.title ?? 'Main Page';
+    data['lang'] = this.lang;
+    data['title'] = this.title;
     return data;
   }
 }
