4f6f70b154919d357c1b311400de35597b76ea81
Micah Teeuws <tutmirage@gmail.com>
2023-12-10 22:54:30 +0300

    Update wiki_link.dart

--- lib/src/wiki/wiki_link.dart
+++ lib/src/wiki/wiki_link.dart
@@ -2,8 +2,18 @@ class WikiLink {
   final String lang;
   final String title;
 
-  WikiLink({
+  const WikiLink({
     required this.lang = 'en',
     required this.title = 'Main Page'
   });
+
+  WikiLink copyWith({
+    String? lang,
+    String? title,
+  }) {
+    return WikiLink(
+      lang: lang ?? this.lang,
+      title: title ?? this.title,
+    );
+  }
 }
