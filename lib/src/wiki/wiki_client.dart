c9b02a55b0497da2db3849c91262e4e9e560c419
Micah Teeuws <tutmirage@gmail.com>
2023-12-11 22:08:48 +0300

    Update wiki

---
+++ lib/src/wiki/wiki_client.dart
@@ -0,0 +1,17 @@
+import 'dart:core';
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+
+import 'category_info.dart';
+import 'wiki_link.dart';
+
+class WikiClient extends GetxService {
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+}
