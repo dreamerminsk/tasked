288c503d8cde000acff63903b85e5cf151ed55f9
Micah Teeuws <tutmirage@gmail.com>
2023-12-11 22:07:08 +0300

    Update controllers

---
+++ lib/src/controllers/cat_controller.dart
@@ -0,0 +1,17 @@
+import 'dart:core';
+import 'dart:async';
+
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+
+import '../wiki/category_info.dart';
+import '../wiki/wiki_link.dart';
+
+class CatController extends GetxController {
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+}
