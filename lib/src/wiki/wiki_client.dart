c8ee5b5b698f1e04e1a949f1f13c70a4e7460edc
Micah Teeuws <tutmirage@gmail.com>
2023-12-14 22:13:44 +0300

    Update wiki_client.dart

--- lib/src/wiki/wiki_client.dart
+++ lib/src/wiki/wiki_client.dart
@@ -1,11 +1,6 @@
 import 'dart:core';
-import 'dart:async';
 
-import 'package:dio/dio.dart';
 import 'package:get/get.dart';
-
-import 'category_info.dart';
-import 'wiki_link.dart';
 
 class WikiClient extends GetxService {
 
