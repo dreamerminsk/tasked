155af3c04342f34a3fc52d212e3f5b9fc524761e
Micah Teeuws <tutmirage@gmail.com>
2023-12-15 18:38:27 +0300

    Update app_pages.dart

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -1,18 +1,23 @@
 class AppPages {
- AppPages._();
+  AppPages._();
 
- static const INITIAL = Routes.CATS;
+  static const INITIAL = Routes.CATS;
 
- static final routes = [
-   GetPage(
-     name: _Paths.HOME,
-     page: () => HomeView(),
-     binding: HomeBinding(),
-   ),
-   GetPage(
-     name: _Paths.CATS,
-     page: () => CatsView(),
-     binding: CatsBinding(),
+  static final routes = [
+    GetPage(
+      name: _Paths.HOME,
+      page: () => HomeView(),
+      binding: HomeBinding(),
+    ),
+    GetPage(
+      name: _Paths.CATS,
+      page: () => CatsView(),
+      binding: CatsBinding(),
+    ),
+    GetPage(
+      name: _Paths.CAT,
+      page: () => CatView(),
+      binding: CatBinding(),
    ),
  ];
 }
