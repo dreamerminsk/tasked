4ff739e55a066e524c9b97328d6bd5712e18e78b
Micah Teeuws <tutmirage@gmail.com>
2023-12-15 18:32:59 +0300

    Update app_routes.dart

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -2,10 +2,10 @@ abstract class Routes {
  Routes._();
 
  static const HOME = _Paths.HOME;
- static const DETAILS = _Paths.DETAILS;
+ static const CATS = _Paths.CATS;
 }
 
 abstract class _Paths {
  static const HOME = '/home';
- static const DETAILS = '/details';
+ static const CATS = '/cats';
 }
