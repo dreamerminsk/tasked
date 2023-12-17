ca6d1354bb7f680a4b7ea5c6a6ceb927ed6e5e5c
Micah Teeuws <tutmirage@gmail.com>
2023-12-17 22:39:48 +0300

    Update watchlist

---
+++ lib/src/watchlist/watchlist_view.dart
@@ -0,0 +1,95 @@
+import 'package:async/async.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'watchlist_controller.dart';
+import '../wiki/category_info.dart';
+import '../views/debug_view.dart';
+import '../views/home_view.dart';
+
+class CatsView extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    final CatsController c = Get.put(CatsController());
+
+    return Scaffold(
+      appBar: AppBar(title: Text("WikiCats"),
+              actions: <Widget>[
+                IconButton(icon: Icon(Icons.home), onPressed: () { Get.to(HomeView()); },),
+                IconButton(icon: Icon(Icons.query_stats), onPressed: () { Get.to(DebugView()); },),
+              ]),
+
+      body: Obx( () =>
+                c.categories.length > 0
+                ? ListView.builder(
+                  itemCount: c.categories.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return _catOrErrorCard(c.categories[index]);
+                  },
+                )
+                : const Center(child: Text('No items'))
+               ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: _buttons(c),
+    );
+  }
+
+  Widget _catOrErrorCard(Result<CategoryInfo> result) {
+    switch (result) {
+      case ErrorResult e:
+        return _errorCard(e.error);
+      case ValueResult v:
+        return _catCard(v.value);
+      default:
+        return Text('very strange');
+    }
+  }
+
+  Widget _errorCard(Object e) {
+    return InkWell(
+      onTap: () { },
+      child: Card(
+        child: ListTile(
+          leading: Icon(Icons.error, color: Colors.red, size: 64.0),
+          title: Text('$e'),
+          subtitle: Text('...')
+        ),
+      ),
+    );
+  }
+
+  Widget _catCard(CategoryInfo cat) {
+    return InkWell(
+      onTap: () { },
+      child: Card(
+        child: ListTile(
+          leading: Icon(Icons.tips_and_updates, size: 64.0),
+          title: Text('${cat.title}'),
+          subtitle: Text('${cat.pages} pages, ${cat.subcats} categories')
+        ),
+      ),
+    );
+  }
+
+  Widget _buttons(CatsController c) {
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FloatingActionButton(
+              heroTag: null,
+              onPressed: () { c.before(); },
+              child: Icon(Icons.navigate_before),
+            ),
+            FloatingActionButton(
+              heroTag: null,
+              onPressed: () { c.after(); },
+              child: Icon(Icons.navigate_next),
+            )
+          ],
+        ),
+      );
+  }//_buttons
+}
