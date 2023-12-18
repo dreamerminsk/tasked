27c1bb736fc9a212db72a900221df2520668f567
Micah Teeuws <tutmirage@gmail.com>
2023-12-18 17:57:35 +0300

    Update home_controller.dart

--- lib/src/home/home_controller.dart
+++ lib/src/home/home_controller.dart
@@ -7,7 +7,7 @@ import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:html/parser.dart';
 
-import '../models/anime.dart';
+import 'entities/anime.dart';
 
 class HomeController extends GetxController {
   static final animeRef = 'https://raw.githubusercontent.com/dreamerminsk/kb-dart/master/data/2023.anime.json';
