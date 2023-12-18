2b4088b1dc30cf79452b99fc62e25dc007247c38
Micah Teeuws <tutmirage@gmail.com>
2023-12-18 23:29:58 +0300

    Update wiki_client.dart

--- lib/src/wiki/wiki_client.dart
+++ lib/src/wiki/wiki_client.dart
@@ -5,7 +5,13 @@ import 'package:get/get.dart';
 
 class WikiClient extends GetxService {
 
-  final Dio _dio = Dio();
+  final Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: 'https://reqres.in/api',
+      connectTimeout: 5000,
+      receiveTimeout: 3000,
+    ),
+  );
 
   @override
   void onInit() {
