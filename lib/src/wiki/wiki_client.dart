c06d2e75977439c295d0678e1a7a21b1be669b2a
Micah Teeuws <tutmirage@gmail.com>
2023-12-19 00:25:46 +0300

    Update wiki_client.dart

--- lib/src/wiki/wiki_client.dart
+++ lib/src/wiki/wiki_client.dart
@@ -5,13 +5,38 @@ import 'package:get/get.dart';
 
 class WikiClient extends GetxService {
 
-  final Dio _dio = Dio(
-    BaseOptions(
-      baseUrl: 'https://reqres.in/api',
-      connectTimeout: 5000,
-      receiveTimeout: 3000,
-    ),
-  );
+  late Dio dio;
+
+  static final WikiClient _instance = WikiClient._internal();
+
+  factory WikiClient() {
+    return _instance;
+  }
+
+  WikiClient._internal() {
+    BaseOptions options = BaseOptions(
+        //baseUrl: AppConstants.SERVER_API_URL,
+        connectTimeout: const Duration(seconds: 32),
+        receiveTimeout: const Duration(seconds: 32),
+        headers: {},
+        contentType: "application/json: charset=utf-8",
+        responseType: ResponseType.json);
+    dio = Dio(options);
+
+    dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) {
+        return handler.next(options);
+      }, onResponse: (response, handler) {
+        if (kDebugMode) {
+          print("app response data ${response.data}");
+        }
+        return handler.next(response);
+      }, onError: (DioException e, handler) {
+        if (kDebugMode) {
+          print("app error data $e");
+        }
+      }));
+  }
 
   @override
   void onInit() {
