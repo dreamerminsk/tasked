454bce7bb14c2d97c4e92b917d553c98fad2ce3b
Micah Teeuws <tutmirage@gmail.com>
2023-12-24 02:06:12 +0300

    Update debug_view.dart

--- lib/src/debug/debug_view.dart
+++ lib/src/debug/debug_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+//import 'package:get/get.dart';
 
-import 'debug_controller.dart';
+//import 'debug_controller.dart';
 
 class DebugView extends StatelessWidget {
 
