41703e53952dc63177c775baa1a3eecbf8205361
Micah Teeuws <tutmirage@gmail.com>
2023-12-24 01:46:06 +0300

    Update politics_view.dart

--- lib/src/politics/politics_view.dart
+++ lib/src/politics/politics_view.dart
@@ -5,40 +5,29 @@ import 'package:flutter_json_viewer/flutter_json_viewer.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../controllers/home_controller.dart';
+import 'politics_controller.dart';
 
 class PoliticsView extends StatelessWidget {
 
   @override
   Widget build(context) {
-    final HomeController c = Get.find();
+    final PoliticsController c = Get.find();
 
     return Scaffold(
-      appBar: AppBar(title: Obx(() => Text("Anime: ${c.animeList.length}")),
+      appBar: AppBar(title: Text('Politics'),
               actions: <Widget>[
                 IconButton(icon: Icon(Icons.copy),
                   onPressed: () { 
-                    c.copyToClipboard();
+                    
                   },
                 )
               ]),
 
-      body: Obx( () {
-        final encoder = JsonEncoder.withIndent('   ');
-        return _scrollable(encoder.convert(c.animeList));
-      }),
+      body: Center(
+          child: Text('NOT IMPLEMENTED'),
+        ),
     );
   }
 
-  Widget _scrollable(String text) {
-    return Row(children: <Widget>[
-             Expanded(
-               child: new SingleChildScrollView(
-                 scrollDirection: Axis.vertical,
-                   child: JsonViewer(jsonDecode(text)),
-               ),
-             ),]
-           );
-  }
 }
 
