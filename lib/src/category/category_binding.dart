1c3ad7eef4acab3bdf0f164535a1f2459530b3c9
Micah Teeuws <tutmirage@gmail.com>
2023-12-23 23:29:40 +0300

    Update category_binding.dart

--- lib/src/category/category_binding.dart
+++ lib/src/category/category_binding.dart
@@ -5,6 +5,7 @@ import 'category_controller.dart';
 class CategoryBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<CategoryController>(() => CategoryController());
+    Get.lazyPut<CategoryController>(
+      () => CategoryController());
   }
 }
