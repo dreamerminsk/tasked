6bdb0389265476da596e1736b4232be25b7eb0ed
Micah Teeuws <tutmirage@gmail.com>
2023-12-24 00:28:32 +0300

    Update category_view.dart

--- lib/src/category/category_view.dart
+++ lib/src/category/category_view.dart
@@ -17,8 +17,8 @@ class AnimeDetailsView extends StatelessWidget {
         Stack(
           children: <Widget>[
             Positioned(
-              top: 20.0,
-              left: 50.0,
+              top: 0.0,
+              left: 0.0,
               child: Expanded(
                 child: Text(
                   'Category:*',
