abaa1f3fd1d6b8ff126f481b75c0a4d00b592296
Micah Teeuws <tutmirage@gmail.com>
2023-12-23 23:12:37 +0300

    Update app_routes.dart

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -4,14 +4,18 @@ abstract class Routes {
  Routes._();
 
   static const HOME = _Paths.HOME;
+  static const DETAILS = _Paths.DETAILS;
   static const WATCHLIST = _Paths.WATCHLIST;
   static const CATEGORY = _Paths.CATEGORY;
   static const DEBUG = _Paths.DEBUG;
+  static const POLITICS = _Paths.POLITICS;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
+  static const DETAILS = '/details';
   static const WATCHLIST = '/watchlist';
   static const CATEGORY = '/category';
   static const DEBUG = '/debug';
+  static const POLITICS = '/politics';
 }
