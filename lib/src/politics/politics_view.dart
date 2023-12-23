809f84241f69dce47e5b5421937aac00f6fd5393
Micah Teeuws <tutmirage@gmail.com>
2023-12-24 02:06:34 +0300

    Update politics_view.dart

--- lib/src/politics/politics_view.dart
+++ lib/src/politics/politics_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+//import 'package:get/get.dart';
 
-import 'politics_controller.dart';
+//import 'politics_controller.dart';
 
 class PoliticsView extends StatelessWidget {
 
