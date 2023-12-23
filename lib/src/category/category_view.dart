d493452a765b9216e6017a80b46b615d7cc4f16e
Micah Teeuws <tutmirage@gmail.com>
2023-12-23 15:17:13 +0300

    Update category

---
+++ lib/src/category/category_view.dart
@@ -0,0 +1,34 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:loading_indicator/loading_indicator.dart';
+
+import '../controllers/category_controller.dart';
+
+class AnimeDetailsView extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    final CategoryController c = Get.find();
+
+    return Scaffold(
+      body: 
+      Obx(()=>
+        Stack(
+          children: <Widget>[
+            Positioned(
+              top: 512.0,
+              left: 50.0,
+              child: Expanded(
+                child: Text(
+                  '<~~~~~>',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+              ), //Expanded
+            ),
+          ],
+        )), //Stack
+      floatingActionButton:
+          FloatingActionButton(child: Icon(Icons.refresh), onPressed: c.refresh));
+  }
+}
