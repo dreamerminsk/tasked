c69e472e39d527dffacefcc3b13cac33253ff22b
Micah Teeuws <tutmirage@gmail.com>
2023-12-23 15:28:03 +0300

    Update app_pages.dart

--- lib/src/routes/app_pages.dart
+++ lib/src/routes/app_pages.dart
@@ -1,3 +1,10 @@
+import '../category/category_binding.dart';
+import '../category/category_view.dart';
+import '../home/home_binding.dart';
+import '../home/home_view.dart';
+import '../watchlist/watchlist_binding';
+import '../watchlist/watchlist_view.dart';
+
 part 'app_routes.dart';
 
 class AppPages {
