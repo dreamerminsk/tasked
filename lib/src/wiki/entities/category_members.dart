b3561764bb6ffc1a3f06bbfc21df98a0d963df17
Micah Teeuws <tutmirage@gmail.com>
2023-12-30 09:58:28 +0300

    Update category_members.dart

--- lib/src/wiki/entities/category_members.dart
+++ lib/src/wiki/entities/category_members.dart
@@ -2,22 +2,26 @@ class CategoryMembers {
   final int pageid;
   final String lang;
   final String title;
+  final DateTime timestamp;
 
   const CategoryMembers({
     this.pageid = 0,
     this.lang = 'en',
     this.title = 'Main Page',
+    this.timestamp,
   });
 
   CategoryMembers copyWith({
     int? pageid,
     String? lang,
     String? title,
+    DateTime? timestamp,
   }) {
     return CategoryMembers(
       pageid: pageid ?? this.pageid,
       lang: lang ?? this.lang,
       title: title ?? this.title,
+      timestamp: timestamp ?? this.timestamp,
     );
   }
 
@@ -26,6 +30,7 @@ class CategoryMembers {
       pageid: json['pageid'] ?? 0,
       lang: json['lang'] ?? 'en',
       title: json['title'] ?? '',
+      timestamp: json['timestamp'] ?? null,
     );
   }
 
