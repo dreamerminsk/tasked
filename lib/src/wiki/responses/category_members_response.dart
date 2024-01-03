8a95d6268872836aebe4bc0e559fe82640d46471
Micah Teeuws <tutmirage@gmail.com>
2024-01-03 15:09:18 +0300

    Update category_members_response.dart

--- lib/src/wiki/responses/category_members_response.dart
+++ lib/src/wiki/responses/category_members_response.dart
@@ -3,47 +3,15 @@ class CategoryMembersResponse {
   final List<CategoryMember> query = [];
 
   CategoryMembersResponse.fromJson(Map<String, dynamic> json) {
-		//continue = json['continue'] != null ? new Continue.fromJson(json['continue']) : null;
-		query = json['query'] != null ? new Query.fromJson(json['query']) : null;
-	}
-}
-
-class Continue {
-	String? cmcontinue;
-	String? continue;
-
-	Continue({this.cmcontinue, this.continue});
-
-	Continue.fromJson(Map<String, dynamic> json) {
-		cmcontinue = json['cmcontinue'];
-		continue = json['continue'];
+		query = json['query'] != null
+  ? _fromJson(json['query'] as Map) : null;
 	}
 
-	Map<String, dynamic> toJson() {
-		final Map<String, dynamic> data = new Map<String, dynamic>();
-		data['cmcontinue'] = this.cmcontinue;
-		data['continue'] = this.continue;
-		return data;
-	}
-}
-
-class Query {
-	List<Categorymembers>? categorymembers;
-
-	Query({this.categorymembers});
-
-	Query.fromJson(Map<String, dynamic> json) {
-		if (json['categorymembers'] != null) {
-			categorymembers = <Categorymembers>[];
-			json['categorymembers'].forEach((v) { categorymembers!.add(new Categorymembers.fromJson(v)); });
-		}
-	}
-
-	Map<String, dynamic> toJson() {
-		final Map<String, dynamic> data = new Map<String, dynamic>();
-		if (this.categorymembers != null) {
-      data['categorymembers'] = this.categorymembers!.map((v) => v.toJson()).toList();
-    }
-		return data;
-	}
+  List<CategoryMember> _fromJson(Map query) {
+    return query['categorymembers'] != null
+    ? query['categorymembers'].map(
+      (item) => CategoryMember.fromJson(item)
+    ).toList()
+    : [];
+  }
 }
