5b074c6dc3368df7bf11b8bc82aa54e835c078e3
Micah Teeuws <tutmirage@gmail.com>
2024-01-05 19:08:03 +0300

    Update debug_view.dart

--- lib/src/debug/debug_view.dart
+++ lib/src/debug/debug_view.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
-//import 'package:get/get.dart';
+import 'package:get/get.dart';
 
-//import 'debug_controller.dart';
+import 'debug_controller.dart';
 
 class DebugView extends StatelessWidget {
 
   @override
   Widget build(context) {
-    //final DebugController c = Get.find();
+    final DebugController c = Get.find();
 
     return Scaffold(
       appBar: AppBar(
-              title: const Text('DEBUG'),
-              actions: <Widget>[
-                IconButton(icon: Icon(Icons.copy),
-                  onPressed: () { 
+        title: const Text('DEBUG'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.copy),
+            onPressed: () { 
                     
-                  },
-                )
-              ]),
+            },
+          )
+        ]),
 
-      body: const Center(child: Text('NOT IMPLEMENTED')),
+      body: Container(
+        width: Get.width,
+        height: 200,
+        child: Center(
+          child: Text('NOT IMPLEMENTED',),
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ), // BorderRadius
+          color: Theme.of(context).colorScheme.secondaryContainer,
+        ), // BoxDecoration
+      ), //Container
     );
   }
 
