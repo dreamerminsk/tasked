9bc767b194bfd02de54f14543e40969056d957cf
Micah Teeuws <tutmirage@gmail.com>
2024-01-06 11:31:22 +0300

    Update health_widget.dart

--- lib/src/debug/widgets/health_widget.dart
+++ lib/src/debug/widgets/health_widget.dart
@@ -1,3 +1,10 @@
 class HealthWidget extends StatelessWidget {
+const HealthWidget({
+    super.key
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return ColoredBox(color: color, child: child);
+  }
 }
