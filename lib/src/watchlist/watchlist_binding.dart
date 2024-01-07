fa4c3a9120ce7442bfb2541faef7747ea2ef80c9
Micah Teeuws <tutmirage@gmail.com>
2024-01-07 14:09:48 +0300

    Update watchlist_binding.dart

--- lib/src/watchlist/watchlist_binding.dart
+++ lib/src/watchlist/watchlist_binding.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
 
 import 'watchlist_controller.dart';
+import '../debug/debug_controller.dart';
 
 class WatchlistBinding implements Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<WatchlistController>(() => WatchlistController());
+    Get.lazyPut<DebugController>(
+      () => DebugController());
+    Get.lazyPut<WatchlistController>(
+      () => WatchlistController());
   }
 }
