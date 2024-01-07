c6734f12e3866fd1bd0d636dbd8c01462410525a
Micah Teeuws <tutmirage@gmail.com>
2024-01-07 20:34:15 +0300

    Update debug_view.dart

--- lib/src/debug/debug_view.dart
+++ lib/src/debug/debug_view.dart
@@ -28,7 +28,7 @@ class DebugView extends StatelessWidget {
         children: <Widget>[
           Obx(() => HealthWidget(
             started: c.started.value?.toString() ?? 'NOT IMPLEMENTED',
-            requests: c.requests,
+            requests: c.requests.value,
           )),
         ],
       ), // ListView
