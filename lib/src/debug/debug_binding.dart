516125706c99036575248b2b93a90565462afc0d
Micah Teeuws <tutmirage@gmail.com>
2024-01-09 08:23:20 +0300

    Update debug_binding.dart

--- lib/src/debug/debug_binding.dart
+++ lib/src/debug/debug_binding.dart
@@ -5,11 +5,11 @@ import 'debug_controller.dart';
 class DebugBinding implements Bindings {
   @override
   void dependencies() {
-    //if (!Get.isRegistered<DebugController>(tag: 'debugger')) {
+    if (!Get.isRegistered<DebugController>(tag: 'debugger')) {
       Get.put<DebugController>(
         DebugController(),
         tag: 'debugger',
       );
-    //}
+    }
   }
 }
