31be8e9a47fc21b8cbb09a9e5a2fcb1a02ff1486
Micah Teeuws <tutmirage@gmail.com>
2024-01-11 23:40:53 +0300

    Update debug

---
+++ lib/src/debug/colorscheme/color_scheme_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//import '../debug_controller.dart';
+
+class ColorSchemePage extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    //final DebugController c = Get.find(tag: 'debugger');
+    //final textTheme = Theme.of(context).textTheme;
+    //final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      body: ListView(
+        //padding: EdgeInsets.all(8),
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Material(
+              elevation: 4,
+              child: Container(
+                width: Get.width,
+                height: 96,
+                child: Placeholder(),
+              ),
+            ),
+          ), // Padding
+        ],
+      ), // ListView
+    );
+  }
+
+}
