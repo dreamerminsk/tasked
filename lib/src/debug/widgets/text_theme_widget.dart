a54cff702f062d6fbb430d839e95cc4ce327df4b
Micah Teeuws <tutmirage@gmail.com>
2024-01-11 16:05:52 +0300

    Update widgets

---
+++ lib/src/debug/widgets/text_theme_widget.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class TextThemeWidget extends StatelessWidget {
+  
+  const TextThemeWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Material(
+      elevation: 1,
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      child: Container(
+        width: Get.width,
+        height: 100,
+        child: Column(
+          crossAxisAlignment:
+            CrossAxisAlignment.center,
+          mainAxisAlignment:
+            MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'textThemeViewer',
+              style: textTheme.headlineSmall!
+                .copyWith(
+                  color: colorScheme.onSurfaceVariant
+                ),
+            ), // Text
+          ],
+        ), // Column
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ), // BorderRadius
+          color: colorScheme.surfaceVariant,
+        ), // BoxDecoration
+      ), //Container
+    ); // Material
+  }
+}
