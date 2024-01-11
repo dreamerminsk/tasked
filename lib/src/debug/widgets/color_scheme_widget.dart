9247418ff116572bc164665ec072936266ca52e8
Micah Teeuws <tutmirage@gmail.com>
2024-01-11 15:20:39 +0300

    Update color_scheme_widget.dart

--- lib/src/debug/widgets/color_scheme_widget.dart
+++ lib/src/debug/widgets/color_scheme_widget.dart
@@ -12,31 +12,35 @@ class ColorSchemeWidget extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    return Container(
-      width: Get.width,
-      height: 100,
-      child: Column(
-        crossAxisAlignment:
-          CrossAxisAlignment.center,
-        mainAxisAlignment:
-          MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            'coloSchemeViewer',
-            style: textTheme.headlineSmall!
-              .copyWith(
-                color: colorScheme.onTertiaryContainer
-              ),
-          ), // Text
-        ],
-      ), // Column
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(
-          Radius.circular(20),
-        ), // BorderRadius
-        color: colorScheme.tertiaryContainer,
-      ), // BoxDecoration
-    ); //Container
+    return Material(
+      elevation: 1,
+      borderRadius: BorderRadius.all(Radius.circular(20)),
+      Container(
+        width: Get.width,
+        height: 100,
+        child: Column(
+          crossAxisAlignment:
+            CrossAxisAlignment.center,
+          mainAxisAlignment:
+            MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'coloSchemeViewer',
+              style: textTheme.headlineSmall!
+                .copyWith(
+                  color: colorScheme.onTertiaryContainer
+                ),
+            ), // Text
+          ],
+        ), // Column
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ), // BorderRadius
+          color: colorScheme.tertiaryContainer,
+        ), // BoxDecoration
+      ), //Container
+    ); // Material
   }
 }
