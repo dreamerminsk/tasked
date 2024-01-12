26ce6095cc491dc0bba60d5807fb36a107ad4d55
Micah Teeuws <tutmirage@gmail.com>
2024-01-12 21:13:48 +0300

    Create json_viewer.dart

---
+++ lib/src/debug/json/json_viewer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//import 'package:get/get.dart';
+
+//import 'debug_controller.dart';
+
+class JsonViewer extends StatelessWidget {
+
+  @override
+  Widget build(context) {
+    //final DebugController c = Get.find(tag: 'debugger');
+    //final textTheme = Theme.of(context).textTheme;
+    //final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: 'json-viewer',
+      ),
+
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white,
+      ),
+    );
+  }
+
+}
