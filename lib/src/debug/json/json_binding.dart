69267d3d87ac0f2230a0ce22f668771a789423c6
Micah Teeuws <tutmirage@gmail.com>
2024-01-12 22:49:57 +0300

    Create json_binding.dart

---
+++ lib/src/debug/json/json_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'json_controller.dart';
+
+class JsonBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<JsonController>(
+        JsonController(),
+      );
+  }
+}
