a564c620d43a479d26175ac5e9bd575c9653293f
Micah Teeuws <tutmirage@gmail.com>
2024-01-14 01:29:06 +0300

    Update watchlist_controller.dart

--- lib/src/watchlist/watchlist_controller.dart
+++ lib/src/watchlist/watchlist_controller.dart
@@ -10,6 +10,9 @@ import '../wiki/entities/wiki_link.dart';
 
 final links = <WikiLink>[
     WikiLink(
+      prefix: 'ru',
+      title: 'Категория:Пришедшие к власти в 2023 году'),
+    WikiLink(
       prefix: 'en',
       title: 'Category:Dogs'),
     WikiLink(
