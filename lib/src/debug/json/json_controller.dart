e9fe18548e8d0558903cec10ec9e57e7ce258888
Micah Teeuws <tutmirage@gmail.com>
2024-01-14 21:32:28 +0300

    Update json_controller.dart

--- lib/src/debug/json/json_controller.dart
+++ lib/src/debug/json/json_controller.dart
@@ -1,6 +1,8 @@
 import 'dart:core';
 
 import 'package:get/get.dart';
+import 'package:dio/dio.dart';
+import 'package:async/async.dart';
 
 class JsonController extends GetxController {
   
@@ -20,6 +22,26 @@ class JsonController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  Future<Result<String>> fetchString(String link) async {
+    try {
+      debug.newReq();
+      var bytes = 0;
+      var ttl = 0;
+      final dio.Response<String> response = await dio.Dio().get(link,
+          onReceiveProgress: (received, total) {
+            bytes = received;
+            ttl = (total > 0) ? total : received;
+          }
+      );
+      debug.newBytes(bytes);
+      debug.newRes({'time': DateTime.now(), 'total': ttl});
+      return Result.value(response.data.toString());
+    } catch (e, s) {
+      Get.snackbar('JsonController.fetchString', '$e', snackPosition: SnackPosition.BOTTOM);
+      return Result.error(e, s);
+    }
   }
 
 }
