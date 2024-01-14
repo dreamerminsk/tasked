c9f44923bb6630ce4d147f205876a84d3bba907e
Micah Teeuws <tutmirage@gmail.com>
2024-01-14 23:55:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,21 +5,24 @@ import 'package:get/get.dart';
 
 import 'src/debug/debug_binding.dart';
 import 'src/routes/app_pages.dart';
+import 'src/core/colors.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  //static NamedColor seedColor;
+  static NamedMaterialColor seedColor;
   
   @override
   Widget build(BuildContext context) {
+    seedColor = NamedColors.primaries[Random().nextInt(NamedColors.primaries.length)];
+    
     return GetMaterialApp(
           title: 'kb-app',
           theme: ThemeData(
             useMaterial3: true,
             //primarySwatch: Colors.indigo,
             colorScheme: ColorScheme.fromSeed(
-              seedColor: Colors.primaries[Random().nextInt(Colors.primaries.length)],
+              seedColor: seedColor.primary,
             ),
           ),
           initialBinding: DebugBinding(),
