af136e6fbc0a438f6745d117b9d75669c3cfad28
Micah Teeuws <tutmirage@gmail.com>
2024-01-16 23:26:49 +0300

    Update text_theme_page.dart

--- lib/src/debug/theme/text_theme_page.dart
+++ lib/src/debug/theme/text_theme_page.dart
@@ -165,7 +165,7 @@ class SampleStyle extends StatelessWidget {
               ),
             ), // Text,
             Text(
-              "${style.fontFamily ?? '.'}, ${style.fontSize ?? 0.0} em",
+              "${style.fontFamily ?? '.'}, ${style.fontSize ?? 0.0} em, ${style.debugLabel}",
               style: textTheme.bodyLarge!
                 .copyWith(
                   color: colorScheme.onPrimary,
