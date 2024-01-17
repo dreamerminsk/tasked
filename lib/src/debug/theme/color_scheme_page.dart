5d18395ca3923bf1543fb9f863dd3d14d71c9dc7
Micah Teeuws <tutmirage@gmail.com>
2024-01-17 13:22:40 +0300

    Update color_scheme_page.dart

--- lib/src/debug/theme/color_scheme_page.dart
+++ lib/src/debug/theme/color_scheme_page.dart
@@ -145,6 +145,7 @@ class SampleColor extends StatelessWidget {
         height: 100,
         alignment: Alignment.bottomCenter,
         child: Container(
+          padding: EdgeInsets.all(2),
           decoration: BoxDecoration(
             color: Colors.transparent,
             border: Border.all(
