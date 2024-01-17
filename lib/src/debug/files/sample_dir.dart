a11e3ec2a8f390a9955b214281748a5333b32a70
Micah Teeuws <tutmirage@gmail.com>
2024-01-17 20:41:34 +0300

    Create sample_dir.dart

---
+++ lib/src/debug/files/sample_dir.dart
@@ -0,0 +1,45 @@
+class SampleDir extends StatelessWidget {
+  final String title;
+  final Color background;
+  final Color foreground;
+
+  const SampleDir({
+    required this.title,
+    required this.background,
+    required this.foreground,
+  });
+
+  @override
+  Widget build(context) {
+    final textTheme = Theme.of(context).textTheme;
+    //final colorScheme = Theme.of(context).colorScheme;
+
+    return Material(
+      elevation: 2,
+      borderRadius:
+        BorderRadius.all(Radius.circular(20)),
+      child: Container(
+        width: Get.width,
+        height: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              title,
+              style: textTheme.titleLarge!
+                .copyWith(
+                  color: foreground,
+              ),
+            ), // Text
+          ],
+        ), // Column
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ), // BorderRadius
+          color: background,
+        ), // BoxDecoration
+      ), // Container
+    ); // Material
+  }
+}
