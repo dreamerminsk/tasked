daa1f1fc649a7914d0544b3b9b4f700469e58b4a
Micah Teeuws <tutmirage@gmail.com>
2024-01-17 08:37:48 +0300

    Update watchlist

---
+++ lib/src/watchlist/random/random_binding.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+import 'random_controller.dart';
+
+class RandomBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<RandomController>(
+      () => RandomController());
+  }
+}
