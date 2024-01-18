4d29b600706d6901bb444a7738c65d3356a80535
Micah Teeuws <tutmirage@gmail.com>
2024-01-18 21:43:52 +0300

    Create parent_dir.dart

---
+++ lib/src/debug/files/parent_dir.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//import 'dir_page.dart';
+
+class ParentDir extends StatelessWidget {
+  final String title;
+
+  const ParentDir({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return InkWell(
+      onTap: () {
+        Get.off();
+        //Get.to(DirPage(path: title), preventDuplicates: false);
+      },
+      child: Material(
+        elevation: 1,
+        borderRadius:
+          BorderRadius.all(Radius.circular(20)),
+        child: Container(
+          width: Get.width,
+          height: 100,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                title.split('/').last,
+                style: textTheme.titleLarge!
+                  .copyWith(
+                    color: colorScheme.onPrimary,
+                ),
+              ), // Text
+            ],
+          ), // Column
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(20),
+              bottomRight: Radius.circular(20),
+            ), // BorderRadius
+            color: colorScheme.primary,
+          ), // BoxDecoration
+        ), // Container
+      ), // Material
+    );
+  }
+}
