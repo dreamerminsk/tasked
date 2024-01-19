52c9af094afdffed81f836f24266c450ac4eda63
Micah Teeuws <tutmirage@gmail.com>
2024-01-19 21:17:42 +0300

    Add files via upload

--- lib/src/debug/theme/theme_data_page.dart
+++ lib/src/debug/theme/theme_data_page.dart
@@ -46,6 +46,48 @@ class ThemeDataPage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'bottomAppBarTheme',
+              theme: theme.bottomAppBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'bottomNavigationBarTheme',
+              theme: theme.bottomNavigationBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'bottomSheetTheme',
+              theme: theme.bottomSheetTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'brightness',
+              theme: theme.brightness,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'buttonBarTheme',
+              theme: theme.buttonBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'buttonTheme',
+              theme: theme.buttonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
             child: SampleColor(
               title: 'canvasColor',
               background: theme.canvasColor,
@@ -62,11 +104,67 @@ class ThemeDataPage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'cardTheme',
+              theme: theme.cardTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'checkboxTheme',
+              theme: theme.checkboxTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'chipTheme',
+              theme: theme.chipTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'colorScheme',
+              theme: theme.colorScheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'cupertinoOverrideTheme',
+              theme: theme.cupertinoOverrideTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'dataTableTheme',
+              theme: theme.dataTableTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'datePickerTheme',
+              theme: theme.datePickerTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
             child: SampleColor(
               title: 'dialogBackgroundColor',
               background: theme.dialogBackgroundColor,
               foreground: invert(theme.dialogBackgroundColor),
             ), // SampleColor
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'dialogTheme',
+              theme: theme.dialogTheme,
+            ), // SampleTheme
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
@@ -83,6 +181,62 @@ class ThemeDataPage extends StatelessWidget {
               background: theme.dividerColor,
               foreground: invert(theme.dividerColor),
             ), // SampleColor
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'dividerTheme',
+              theme: theme.dividerTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'drawerTheme',
+              theme: theme.drawerTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'dropdownMenuTheme',
+              theme: theme.dropdownMenuTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'elevatedButtonTheme',
+              theme: theme.elevatedButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'expansionTileTheme',
+              theme: theme.expansionTileTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'extensions',
+              theme: theme.extensions,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'filledButtonTheme',
+              theme: theme.filledButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'floatingActionButtonTheme',
+              theme: theme.floatingActionButtonTheme,
+            ), // SampleTheme
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
@@ -118,11 +272,116 @@ class ThemeDataPage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'iconButtonTheme',
+              theme: theme.iconButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'iconTheme',
+              theme: theme.iconTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
             child: SampleColor(
               title: 'indicatorColor',
               background: theme.indicatorColor,
               foreground: invert(theme.indicatorColor),
             ), // SampleColor
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'inputDecorationTheme',
+              theme: theme.inputDecorationTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'listTileTheme',
+              theme: theme.listTileTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'materialTapTargetSize',
+              theme: theme.materialTapTargetSize,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'menuBarTheme',
+              theme: theme.menuBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'menuButtonTheme',
+              theme: theme.menuButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'menuTheme',
+              theme: theme.menuTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'navigationBarTheme',
+              theme: theme.navigationBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'navigationDrawerTheme',
+              theme: theme.navigationDrawerTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'navigationRailTheme',
+              theme: theme.navigationRailTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'outlinedButtonTheme',
+              theme: theme.outlinedButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'pageTransitionsTheme',
+              theme: theme.pageTransitionsTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'platform',
+              theme: theme.platform,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'popupMenuTheme',
+              theme: theme.popupMenuTheme,
+            ), // SampleTheme
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
@@ -150,11 +409,60 @@ class ThemeDataPage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'primaryIconTheme',
+              theme: theme.primaryIconTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'primaryTextTheme',
+              theme: theme.primaryTextTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'progressIndicatorTheme',
+              theme: theme.progressIndicatorTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'radioTheme',
+              theme: theme.radioTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
             child: SampleColor(
               title: 'scaffoldBackgroundColor',
               background: theme.scaffoldBackgroundColor,
               foreground: invert(theme.scaffoldBackgroundColor),
             ), // SampleColor
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'scrollbarTheme',
+              theme: theme.scrollbarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'searchBarTheme',
+              theme: theme.searchBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'searchViewTheme',
+              theme: theme.searchViewTheme,
+            ), // SampleTheme
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
@@ -166,11 +474,32 @@ class ThemeDataPage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'segmentedButtonTheme',
+              theme: theme.segmentedButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
             child: SampleColor(
               title: 'shadowColor',
               background: theme.shadowColor,
               foreground: invert(theme.shadowColor),
             ), // SampleColor
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'sliderTheme',
+              theme: theme.sliderTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'snackBarTheme',
+              theme: theme.snackBarTheme,
+            ), // SampleTheme
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
@@ -182,11 +511,88 @@ class ThemeDataPage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'splashFactory',
+              theme: theme.splashFactory,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'switchTheme',
+              theme: theme.switchTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'tabBarTheme',
+              theme: theme.tabBarTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'textButtonTheme',
+              theme: theme.textButtonTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'textSelectionTheme',
+              theme: theme.textSelectionTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'textTheme',
+              theme: theme.textTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'timePickerTheme',
+              theme: theme.timePickerTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'toggleButtonsTheme',
+              theme: theme.toggleButtonsTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'tooltipTheme',
+              theme: theme.tooltipTheme,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'typography',
+              theme: theme.typography,
+            ), // SampleTheme
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
             child: SampleColor(
               title: 'unselectedWidgetColor',
               background: theme.unselectedWidgetColor,
               foreground: invert(theme.unselectedWidgetColor),
             ), // SampleColor
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleTheme(
+              title: 'visualDensity',
+              theme: theme.visualDensity,
+            ), // SampleTheme
           ), // Padding
         ],
       ), // ListView
