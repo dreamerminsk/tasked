7d2bda31df33197f8b52686f5d394bccdb59f751
Micah Teeuws <tutmirage@gmail.com>
2024-01-23 14:58:20 +0300

    Update home_details_view.dart

--- lib/src/home/home_details_view.dart
+++ lib/src/home/home_details_view.dart
@@ -6,6 +6,7 @@ import 'package:loading_indicator/loading_indicator.dart';
 import 'home_controller.dart';
 
 //https://en.wikipedia.org/api/rest_v1/page/summary/Minsk
+//https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&pageids=21721040
 class HomeDetailsView extends StatelessWidget {
 
   @override
