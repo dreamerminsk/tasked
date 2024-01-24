e14041d5e19d605372902ee40022774ff8b079ce
Micah Teeuws <tutmirage@gmail.com>
2024-01-24 20:52:54 +0300

    Update sample_text_style.dart

--- lib/src/debug/theme/sample_text_style.dart
+++ lib/src/debug/theme/sample_text_style.dart
@@ -16,7 +16,7 @@ class SampleTextStyle extends StatelessWidget {
 
   @override
   Widget build(context) {
-    final textTheme = Theme.of(context).textTheme;
+    //final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
     return Material(
