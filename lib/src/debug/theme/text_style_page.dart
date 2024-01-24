be1bdb4dcdddfbcf5aee2eb45368ed49bb88a12c
Micah Teeuws <tutmirage@gmail.com>
2024-01-24 17:15:22 +0300

    Update text_style_page.dart

--- lib/src/debug/theme/text_style_page.dart
+++ lib/src/debug/theme/text_style_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'sample_object.dart';
+
 class TextStylePage extends StatelessWidget {
 
   final TextStyle textStyle;
@@ -28,101 +30,10 @@ class TextStylePage extends StatelessWidget {
           ), // Padding
           Padding(
             padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'displayMedium',
-              style: textTheme.displayMedium!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'displaySmall',
-              style: textTheme.displaySmall!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'headlineLarge',
-              style: textTheme.headlineLarge!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'headlineMedium',
-              style: textTheme.headlineMedium!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'headlineSmall',
-              style: textTheme.headlineSmall!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'titleLarge',
-              style: textTheme.titleLarge!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'titleMedium',
-              style: textTheme.titleMedium!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'titleSmall',
-              style: textTheme.titleSmall!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'labelLarge',
-              style: textTheme.labelLarge!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'labelMedium',
-              style: textTheme.labelMedium!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'labelSmall',
-              style: textTheme.labelSmall!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'bodyLarge',
-              style: textTheme.bodyLarge!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'bodyMedium',
-              style: textTheme.bodyMedium!
-            ), // TextContainer
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.all(8),
-            child: SampleStyle(
-              title: 'bodySmall',
-              style: textTheme.bodySmall!
-            ), // TextContainer
+            child: SampleObject(
+              title: 'backgroundColor',
+              object: textStyle.backgroundColor,
+            ), // SampleObject
           ), // Padding
         ],
       ), // ListView
