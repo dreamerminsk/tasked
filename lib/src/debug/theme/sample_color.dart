d1b6cf635db983edfd11a143eeb075ae9cbf8949
Micah Teeuws <tutmirage@gmail.com>
2024-01-25 00:47:41 +0300

    Update sample_color.dart

--- lib/src/debug/theme/sample_color.dart
+++ lib/src/debug/theme/sample_color.dart
@@ -21,41 +21,43 @@ class SampleColor extends StatelessWidget {
       elevation: 2,
       borderRadius:
         BorderRadius.all(Radius.circular(20)),
-      child: Container(
-        width: Get.width,
-        height: 100,
-        alignment: Alignment.bottomCenter,
+      child: InkWell(
         child: Container(
-          padding: EdgeInsets.fromLTRB(8, 2, 8, 2),
+          width: Get.width,
+          height: 100,
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(8, 2, 8, 2),
+            decoration: BoxDecoration(
+              color: Colors.transparent,
+              border: Border.all(
+                color: foreground,
+                width: 2.0,
+              ), // Border
+              borderRadius: BorderRadius.circular(12),
+            ), // BoxDecoration
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: textTheme.titleLarge!
+                    .copyWith(
+                      color: foreground,
+                  ),
+                ), // Text
+              ],
+            ), // Column
+          ),
           decoration: BoxDecoration(
-            color: Colors.transparent,
-            border: Border.all(
-              color: foreground,
-              width: 2.0,
-            ), // Border
-            borderRadius: BorderRadius.circular(12),
+            borderRadius: BorderRadius.all(
+              Radius.circular(20),
+            ), // BorderRadius
+            color: background,
           ), // BoxDecoration
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Text(
-                title,
-                style: textTheme.titleLarge!
-                  .copyWith(
-                    color: foreground,
-                ),
-              ), // Text
-            ],
-          ), // Column
-        ),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(
-            Radius.circular(20),
-          ), // BorderRadius
-          color: background,
-        ), // BoxDecoration
-      ), // Container
+        ), // Container
+      ), // InkWell
     ); // Material
   }
 }
