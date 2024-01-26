53487db9e3187cafb827963cac6ea4aacb8a30f4
Micah Teeuws <tutmirage@gmail.com>
2024-01-26 19:19:49 +0300

    Update dir_page.dart

--- lib/src/debug/files/dir_page.dart
+++ lib/src/debug/files/dir_page.dart
@@ -3,26 +3,22 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-//import '../debug_controller.dart';
 import 'parent_dir.dart';
 import 'sample_dir.dart';
 
 class DirPage extends StatelessWidget {
   final path;
-  //final RxString parent = RxString();
   final RxList entries = RxList();
 
   DirPage({super.key, required this.path});
 
   @override
   Widget build(context) {
-    //final DebugController c = Get.find(tag: 'debugger');
     //final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
     if (entries.isEmpty) {
       final d = Directory(path);
-      //parent.value = d.parent.path;
       d.list().map((item) => item.path).forEach((item) => entries.add(item));
     }
     
