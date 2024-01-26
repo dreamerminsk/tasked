c8a81e2eabe5e7781474488635ad3fb25ccb10f3
Micah Teeuws <tutmirage@gmail.com>
2024-01-27 00:03:55 +0300

    Update theme

---
+++ lib/src/debug/theme/button_theme_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import 'sample_object.dart';
+
+class ButtonThemePage extends StatelessWidget {
+
+  final ButtonThemeData theme;
+
+  const ButtonThemePage({
+    super.key,
+    required this.theme,
+  });
+
+  @override
+  Widget build(context) {
+    //final textTheme = Theme.of(context).textTheme;
+    //final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleObject(
+              title: 'alpha',
+              object: color.alpha,
+            ), // SampleObject
+          ), // Padding
+        ],
+      ), // ListView
+    );
+  }
+
+}
