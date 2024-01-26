da63d83ddbeab72a92cb2059dcae134b681f3f1e
Micah Teeuws <tutmirage@gmail.com>
2024-01-26 14:27:18 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,25 @@ void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   static NamedMaterialColor seedColor = NamedColors.indigo;
+  static NamedColor shadeColor = NamedColor.indigo.shade500;
   
   @override
   Widget build(BuildContext context) {
     seedColor = NamedColors.primaries[Random().nextInt(NamedColors.primaries.length)];
+    var shades = [
+      seedColor.shade50,
+      seedColor.shade100,
+      seedColor.shade200,
+      seedColor.shade300,
+      seedColor.shade400,
+      seedColor.shade500,
+      seedColor.shade600,
+      seedColor.shade700,
+      seedColor.shade800,
+      seedColor.shade900,
+    ];
+    shades.shuffle();
+    shadeColor = shades[0];
     
     return GetMaterialApp(
           title: 'kb-app',
@@ -22,7 +37,7 @@ class MyApp extends StatelessWidget {
             useMaterial3: true,
             //primarySwatch: Colors.indigo,
             colorScheme: ColorScheme.fromSeed(
-              seedColor: seedColor.primary,
+              seedColor: shadeColor,
             ),
           ),
           initialBinding: DebugBinding(),
