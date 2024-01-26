e6c2cd7108d77ec2a712a8adf789ca085eb29e9e
Micah Teeuws <tutmirage@gmail.com>
2024-01-26 23:50:59 +0300

    Update theme_data_page.dart

--- lib/src/debug/theme/theme_data_page.dart
+++ lib/src/debug/theme/theme_data_page.dart
@@ -611,59 +611,6 @@ class ThemeDataPage extends StatelessWidget {
 }
 
 
-class SampleTheme extends StatelessWidget {
-  final String title;
-  final Object? theme;
-  
-  const SampleTheme({
-    super.key,
-    required this.title,
-    required this.theme,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return Material(
-      elevation: 2,
-      borderRadius: BorderRadius.all(Radius.circular(20)),
-      child: InkWell(
-        onTap: () {
-          //Get.toNamed(Routes.THEME); 
-        },
-        child: Container(
-          width: Get.width,
-          height: 100,
-          child: Column(
-            mainAxisAlignment:
-              MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                title,
-                style: textTheme.headlineSmall!
-                  .copyWith(
-                    color:
-                      theme == null ?  colorScheme.onSurfaceVariant : colorScheme.onPrimary,
-                  ),
-              ), // Text
-            ],
-          ), // Column
-          padding: const EdgeInsets.all(16),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
-              Radius.circular(20),
-            ), // BorderRadius
-            color: theme == null ?  colorScheme.surfaceVariant : colorScheme.primary,
-          ), // BoxDecoration
-        ), //Container
-      ), // InkWell
-    ); // Material
-  }
-}
-
-
 
 class SampleColor extends StatelessWidget {
   final String title;
