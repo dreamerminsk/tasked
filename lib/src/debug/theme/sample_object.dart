3d00e8dc4c07e636c06e4c7fcd765561d829a888
Micah Teeuws <tutmirage@gmail.com>
2024-01-27 20:24:25 +0300

    Update sample_object.dart

--- lib/src/debug/theme/sample_object.dart
+++ lib/src/debug/theme/sample_object.dart
@@ -28,7 +28,7 @@ class SampleObject extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             Text(
-              '${object == null ? "null" : (object is Diagnosticable ? object!.toStringShort() : object.toString())}',
+              '${object == null ? "null" : (object is Diagnosticable ? (object! as Diagnosticable).toStringShort() : object.toString())}',
               style: textTheme.titleLarge!
                 .copyWith(
                   fontWeight: FontWeight.w700,
