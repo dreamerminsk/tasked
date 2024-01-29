600000c462d7ae5a0b511b1ee00f04ef1eec2981
Micah Teeuws <tutmirage@gmail.com>
2024-01-29 09:32:06 +0300

    Update home_details_view.dart

--- lib/src/home/home_details_view.dart
+++ lib/src/home/home_details_view.dart
@@ -93,7 +93,7 @@ class HomeDetailsView extends StatelessWidget {
               ), // GestureDetector
             ), // Positioned
             Positioned(
-              top: 2 * Get.height / 3,
+              top: Get.height / 3,
               left: 32,
               right: 32,
               child: GestureDetector(
@@ -115,6 +115,7 @@ class HomeDetailsView extends StatelessWidget {
                     ), // BoxDecoration
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
                         Expanded(
                           child: Text(
