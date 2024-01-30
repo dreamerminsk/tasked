6e48b14346f5c92c374ff8e09a644bf1d28647a4
Micah Teeuws <tutmirage@gmail.com>
2024-01-30 23:21:08 +0300

    Update json_object_card.dart

--- lib/src/debug/json/json_object_card.dart
+++ lib/src/debug/json/json_object_card.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'json_controller.dart';
 
 class JsonObjectCard extends StatelessWidget {
+  final JsonController c = Get.find();
+  final textTheme = Theme.of(context).textTheme;
+  final colorScheme = Theme.of(context).colorScheme;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Container(
+        margin: EdgeInsets.all(8.0),
+        width: Get.width,
+        height: 200,
+        child: Row(
+          mainAxisAlignment:
+            MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment:
+                CrossAxisAlignment.start,
+              mainAxisAlignment:
+                MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  'pos: 0',
+                  style: textTheme.bodyLarge!
+                    .copyWith(
+                      color: colorScheme.onPrimary
+                    ),
+                ), // Text
+                Text(
+                  'length: ${c.content.value.length}',
+                  style: textTheme.bodyLarge!
+                    .copyWith(
+                      color: colorScheme.onPrimary
+                    ),
+                ), // Text
+              ],
+            ), // Column
+            Container(
+              height: 200,
+              alignment: Alignment.center,
+              child: RotatedBox(
+                quarterTurns: 3,
+                child: Text('JsonObject',
+                  style: textTheme.titleLarge!
+                    .copyWith(
+                      color: colorScheme.onPrimary
+                    ),
+                ), // Text
+              ), // RotatedBox,
+              decoration: BoxDecoration(
+                border: Border(
+                  left: BorderSide(
+                    color: colorScheme.onPrimary,
+                    width: 1,
+                  ), // BorderSide
+                ), // Border
+              ), // BoxDecoration
+            ), // Container
+          ],
+        ), // Row
+        padding: const EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+            Radius.circular(20),
+          ), // BorderRadius
+          color: colorScheme.primary,
+        ), // BoxDecoration
+      ); //Container
   }
 
 }
