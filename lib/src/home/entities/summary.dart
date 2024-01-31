af6e690abf119d9f5f4bade89dec403bf0b97be4
Micah Teeuws <tutmirage@gmail.com>
2024-01-31 11:33:47 +0300

    Update summary.dart

--- lib/src/home/entities/summary.dart
+++ lib/src/home/entities/summary.dart
@@ -2,34 +2,22 @@ class Summary {
   final int pageid;
   final String lang;
   final String title;
-  final int pages;
-  final int files;
-  final int subcats;
 
   const Summary({
     this.pageid = 0,
     this.lang = 'en',
     this.title = 'Main Page',
-    this.pages = 0,
-    this.files = 0,
-    this.subcats = 0
   });
 
   Summary copyWith({
     int? pageid,
     String? lang,
     String? title,
-    int? pages,
-    int? files,
-    int? subcats,
   }) {
     return Summary(
       pageid: pageid ?? this.pageid,
       lang: lang ?? this.lang,
       title: title ?? this.title,
-      pages: pages ?? this.pages,
-      files: files ?? this.files,
-      subcats: subcats ?? this.subcats,
     );
   }
 
@@ -38,13 +26,10 @@ class Summary {
       pageid: json['pageid'] ?? 0,
       lang: json['lang'] ?? 'en',
       title: json['title'] ?? '',
-      pages: json['categoryinfo']?['pages'] ?? 0,
-      files: json['categoryinfo']?['files'] ?? 0,
-      subcats: json['categoryinfo']?['subcats'] ?? 0,
     );
   }
 
-  (int, String, String, int, int) _equality() => (pageid, lang, title, pages, subcats);
+  (int, String, String) _equality() => (pageid, lang, title);
 
   @override
   bool operator ==(covariant Summary other) {
