76f94ba52414c1cb9e35365ecf413c1b2f9286b7
Micah Teeuws <tutmirage@gmail.com>
2024-01-31 11:36:06 +0300

    Update summary.dart

--- lib/src/home/entities/summary.dart
+++ lib/src/home/entities/summary.dart
@@ -2,22 +2,26 @@ class Summary {
   final int pageid;
   final String lang;
   final String title;
+  final String description;
 
   const Summary({
     this.pageid = 0,
     this.lang = 'en',
     this.title = 'Main Page',
+    this.description = '',
   });
 
   Summary copyWith({
     int? pageid,
     String? lang,
     String? title,
+    String? description,
   }) {
     return Summary(
       pageid: pageid ?? this.pageid,
       lang: lang ?? this.lang,
       title: title ?? this.title,
+      description: description ?? this.description,
     );
   }
 
@@ -26,6 +30,7 @@ class Summary {
       pageid: json['pageid'] ?? 0,
       lang: json['lang'] ?? 'en',
       title: json['title'] ?? '',
+      description: json['description'] ?? '',
     );
   }
 
