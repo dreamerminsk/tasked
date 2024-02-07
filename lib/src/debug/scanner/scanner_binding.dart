d4c8fc548f65c4245456df5a7c81281442e84ffd
Micah Teeuws <tutmirage@gmail.com>
2024-02-07 10:31:44 +0300

    Create scanner_binding.dart

---
+++ lib/src/debug/scanner/scanner_binding.dart
@@ -0,0 +1,12 @@
+import 'package:get/get.dart';
+
+import 'scanner_controller.dart';
+
+class ScannerBinding implements Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<ScannerController>(
+        () => ScannerController(),
+      );
+  }
+}
