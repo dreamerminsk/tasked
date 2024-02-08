59cdf87566948dc6bbc80611e042afec5171a1b9
Micah Teeuws <tutmirage@gmail.com>
2024-02-08 21:53:28 +0300

    Update health_widget.dart

--- lib/src/debug/widgets/health_widget.dart
+++ lib/src/debug/widgets/health_widget.dart
@@ -55,7 +55,7 @@ class HealthWidget extends StatelessWidget {
         .map<Widget>((item) => Text(
               '${item.key}: ${item.value}',
               style:
-                  textTheme.bodyLarge!.copyWith(color: colorScheme.onPrimary),
+                  textTheme.bodyMedium!.copyWith(color: colorScheme.onPrimary),
             ))
         .toList();
   }
