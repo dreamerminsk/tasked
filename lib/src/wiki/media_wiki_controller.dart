b54eaf89280be50991eb0efd8492f755bd6b1e9b
Micah Teeuws <tutmirage@gmail.com>
2024-02-09 21:18:53 +0300

    Update media_wiki_controller.dart

--- lib/src/wiki/media_wiki_controller.dart
+++ lib/src/wiki/media_wiki_controller.dart
@@ -2,12 +2,32 @@ import 'package:get/get.dart';
 import 'media_wiki.dart';
 
 
+import '../debug/debug_controller.dart';
 
+
+  
 
 class MediaWikiController extends GetxService {
+  final DebugController debug = Get.find(tag: 'debugger');
+  
 final MediaWiki mediaWiki;
 
 MediaWikiController({required this.mediaWiki});
+@override
+  void onInit() {
+    super.onInit();
+    debug.newInit(this.runtimeType.toString());
+  }
 
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    debug.newClose(this.runtimeType.toString());
+    super.onClose();
+  }
 
 }
