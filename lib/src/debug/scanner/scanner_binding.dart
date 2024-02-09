ed315cf0a4a0cad270378abf25f534cad67f5019
Micah Teeuws <tutmirage@gmail.com>
2024-02-09 21:11:54 +0300

    Update scanner_binding.dart

--- lib/src/debug/scanner/scanner_binding.dart
+++ lib/src/debug/scanner/scanner_binding.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
-
+import '../../media_wiki_controller.dart';
 import 'scanner_controller.dart';
 
 class ScannerBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<MediaWikiController>(
+      () => MediaWikiController(),
+    );
     Get.lazyPut<ScannerController>(
       () => ScannerController(),
     );
