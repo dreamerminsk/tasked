5624230df71cc03de141172526df0c1e22b1f26e
Micah Teeuws <tutmirage@gmail.com>
2024-05-23 11:20:38 +0300

    Update tasklist_controller.dart

--- lib/src/tasklist/tasklist_controller.dart
+++ lib/src/tasklist/tasklist_controller.dart
@@ -7,17 +7,18 @@ import 'models/task.dart';
 
 class TasklistController extends GetxController {
   final DebugController debug = Get.find(tag: 'debugger');
-  final tasks = new List<Task>.generate(
-      32,
-      (int index) => Task(
-            id: index,
-            title: 'task $index',
-          ));
+  final tasks = <Task>[].obs;
 
   @override
   void onInit() {
     super.onInit();
     debug.newInit(this.runtimeType.toString());
+    tasks.add(new List<Task>.generate(
+      32,
+      (int index) => Task(
+            id: index,
+            title: 'task $index',
+          )));
   }
 
   @override
