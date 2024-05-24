67c1cb5706749e34aa5c50bce633e980a1684531
Micah Teeuws <tutmirage@gmail.com>
2024-05-24 22:15:43 +0300

    Update event_controller.dart

--- lib/src/snooker/event_controller.dart
+++ lib/src/snooker/event_controller.dart
@@ -8,12 +8,13 @@ import '../tasklist/task/task_item.dart';
 
 class EventController extends GetxController {
   final DebugController debug = Get.find(tag: 'debugger');
-  final task = <TaskItem>[].obs;
+  final task = Rxn<TaskItem>();
 
   @override
   void onInit() {
     super.onInit();
     debug.newInit(this.runtimeType.toString());
+    task.value = Get.arguments;
   }
 
   @override
