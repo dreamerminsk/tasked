ec0d0ce822b2cf6aafc184fe930c2fbab7b345ba
Micah Teeuws <tutmirage@gmail.com>
2024-05-24 10:56:25 +0300

    Update src

---
+++ lib/src/snooker/event_view.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+//import 'task_controller.dart';
+import '../routes/app_pages.dart';
+import '../tasklist/task/task_item.dart';
+
+class EventView extends StatelessWidget {
+  const EventView({
+    super.key,
+  });
+
+  @override
+  Widget build(context) {
+    final TaskItem task = Get.arguments;
+
+    return Scaffold(
+      appBar: AppBar(title: Text("${task.title}"), actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.query_stats),
+          color: Theme.of(context).colorScheme.primary,
+          onPressed: () {
+            Get.toNamed(Routes.DEBUG);
+          },
+        ),
+      ]),
+      body: Center(
+        child: Icon(Icons.blur_on,
+            color: Theme.of(context).colorScheme.primary, size: 320.0), // Icon
+      ), // Center
+    );
+  }
+}
