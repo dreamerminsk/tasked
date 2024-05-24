2d2f38f24f66b2e4a0935d79a45a35eba1bdeb3b
Micah Teeuws <tutmirage@gmail.com>
2024-05-24 11:11:47 +0300

    Update task_widget.dart

--- lib/src/tasklist/task/task_widget.dart
+++ lib/src/tasklist/task/task_widget.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import 'task_item.dart';
-import '../../routes/app_pages.dart';
 
 class TaskWidget extends StatelessWidget {
   const TaskWidget({
