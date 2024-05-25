9e23c57b232f90dc54ab2e64befbc2b4ae2c8120
Micah Teeuws <tutmirage@gmail.com>
2024-05-25 20:17:25 +0300

    Update html

---
+++ lib/src/html/html_view.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'html_controller.dart';
+import '../routes/app_pages.dart';
+//import '../tasklist/task/task_item.dart';
+
+class HtmlView extends StatelessWidget {
+  const HtmlView({
+    super.key,
+  });
+
+  @override
+  Widget build(context) {
+    final HtmlController c = Get.find();
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Obx(() => Text("${c.task.value?.title}")),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.query_stats),
+              color: Theme.of(context).colorScheme.primary,
+              onPressed: () {
+                Get.toNamed(Routes.DEBUG);
+              },
+            ),
+          ]),
+      body: Center(
+        child: IconButton.filled(
+          iconSize: 320,
+          icon: const Icon(Icons.blur_on),
+          onPressed: () {},
+        ),
+      ), // Center
+    );
+  }
+}
