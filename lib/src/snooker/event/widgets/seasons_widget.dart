b54c596b02335065c33bf369f20cfb11f75c058d
Micah Teeuws <tutmirage@gmail.com>
2024-05-26 14:20:33 +0300

    Update event

---
+++ lib/src/snooker/event/widgets/seasons_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SeasonsWidget extends StatelessWidget {
+  const SeasonsWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Container(
+        width: Get.width,
+        height: Get.height,
+margin: const EdgeInsets.all(8),
+        child: ListView(), // ListView
+      ); // Container
+  }
+}
