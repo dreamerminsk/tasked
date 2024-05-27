0536ab6984f1cd25fe82fe66bb4479c11d3bd02a
Micah Teeuws <tutmirage@gmail.com>
2024-05-27 21:00:31 +0300

    Update html_controller.dart

--- lib/src/html/html_controller.dart
+++ lib/src/html/html_controller.dart
@@ -6,7 +6,11 @@ import '../debug/debug_controller.dart';
 import '../tasklist/task/task_item.dart';
 
 class HtmlController extends GetxController {
+
   final DebugController debug = Get.find(tag: 'debugger');
+
+final resourceController = TextEditingController('https://www.rottentomatoes.com/');
+
   final task = Rxn<TaskItem>();
 
   @override
