a346af752c8a172b047bc3332e137ff25f92ebef
Micah Teeuws <tutmirage@gmail.com>
2024-05-29 23:47:39 +0300

    Update controller_info.dart

--- lib/src/debug/models/controller_info.dart
+++ lib/src/debug/models/controller_info.dart
@@ -1,3 +1,24 @@
 class ControllerInfo{
-const ControllerInfo;
+final int total;
+final int live;
+const ControllerInfo({this.total =0,this.live=0,});
+ControllerInfo copyWith({
+    int? total,
+    int? live,
+  }) {
+    return ControllerInfo(
+      total: total ?? this.total,
+      live: live ?? this.live,
+    );
+  }
+(int,int) _equality() => (total, live);
+
+  @override
+  bool operator ==(covariant ControllerInfo other) {
+    if (identical(this, other)) return true;
+    return other._equality() == _equality();
+  }
+
+  @override
+  int get hashCode => _equality().hashCode;
 }
