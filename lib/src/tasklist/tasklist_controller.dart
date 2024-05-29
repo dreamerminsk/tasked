46763a5b2665f9f3033014bfa547d8e15ac380a6
Micah Teeuws <tutmirage@gmail.com>
2024-05-30 01:06:58 +0300

    Update tasklist_controller.dart

--- lib/src/tasklist/tasklist_controller.dart
+++ lib/src/tasklist/tasklist_controller.dart
@@ -9,19 +9,24 @@ import 'task/task_item.dart';
 class TasklistController extends GetxController {
   final DebugController debug = Get.find(tag: 'debugger');
   final tasks = <TaskItem>[
-    TaskItem(
+TaskItem(
       id: 0,
-      title: 'snooker :: events',
-      path: Routes.SNOOKEREVENTS,
+      title: 'snooker ::: seasons',
+      path: Routes.SNOOKERSEASONS,
     ),
     TaskItem(
       id: 1,
-      title: 'snooker :: players',
-      path: Routes.SNOOKERPLAYERS,
+      title: 'snooker ::: events',
+      path: Routes.SNOOKEREVENTS,
     ),
     TaskItem(
       id: 2,
-      title: 'html :: viewer',
+      title: 'snooker ::: players',
+      path: Routes.SNOOKERPLAYERS,
+    ),
+    TaskItem(
+      id: 3,
+      title: 'html ::: viewer',
       path: Routes.HTML,
     ),
   ].obs;
@@ -31,10 +36,10 @@ class TasklistController extends GetxController {
     super.onInit();
     debug.newInit(this.runtimeType.toString());
     tasks.addAll(List<TaskItem>.generate(
-        29,
+        28,
         (int index) => TaskItem(
-              id: index + 3,
-              title: 'task ${index + 3}',
+              id: index + 4,
+              title: 'task ${index + 4}',
             )));
   }
 
