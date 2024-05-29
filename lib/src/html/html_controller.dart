108f50758f04bbc1f0f6c2afe9303d9fda784f58
Micah Teeuws <tutmirage@gmail.com>
2024-05-29 10:50:53 +0300

    Update html_controller.dart

--- lib/src/html/html_controller.dart
+++ lib/src/html/html_controller.dart
@@ -14,6 +14,7 @@ class HtmlController extends GetxController {
     'https://www.rottentomatoes.com/',
     'https://en.wikipedia.org/wiki/Main_Page',
     'https://dtf.ru/',
+'https://vc.ru/',
     'https://www.metacritic.com/',
     'https://habr.com/ru/articles/top/daily/',
     'https://tech.onliner.by/',
