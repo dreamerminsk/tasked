b1267a1f377357bf45a210ee8487843bd590adde
Micah Teeuws <tutmirage@gmail.com>
2024-06-03 14:31:52 +0300

    Update event_controller.dart

--- lib/src/snooker/event/event_controller.dart
+++ lib/src/snooker/event/event_controller.dart
@@ -1,11 +1,14 @@
 import 'dart:core';
 
 import 'package:get/get.dart';
+import 'package:nanoid/nanoid.dart';
 
 import '../../debug/debug_controller.dart';
 import '../../tasklist/task/task_item.dart';
 
 class EventController extends GetxController {
+  final id = nanoid();
+  final started = DateTime.now();
   final DebugController debug = Get.find(tag: 'debugger');
   final task = Rxn<TaskItem>();
   var isRunning = false.obs;
