8af05bc6700d1511a06d6dd472df6d3bced4b089
Micah Teeuws <tutmirage@gmail.com>
2024-06-07 13:56:04 +0300

    Update html_controller.dart

--- lib/src/html/html_controller.dart
+++ lib/src/html/html_controller.dart
@@ -3,11 +3,14 @@ import 'dart:core';
 import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:nanoid2/nanoid2.dart';
 
 import '../debug/debug_controller.dart';
 import '../tasklist/task/task_item.dart';
 
 class HtmlController extends GetxController {
+final id = nanoid();
+  final started = DateTime.now();
   final DebugController debug = Get.find(tag: 'debugger');
 
   static final defaultUrls = List.unmodifiable([
@@ -29,13 +32,13 @@ class HtmlController extends GetxController {
   @override
   void onInit() {
     super.onInit();
-    debug.newInit(this.runtimeType.toString());
+debug.newInit(this.runtimeType.toString(),id,started);
     task.value = Get.arguments;
   }
 
   @override
   void onClose() {
-    debug.newClose(this.runtimeType.toString());
+debug.newClose(this.runtimeType.toString(),id,DateTime.now());
     resourceController.dispose();
     super.onClose();
   }
