91a07d191d5f3226b0c036e7abc4d2df73647c75
Micah Teeuws <tutmirage@gmail.com>
2024-06-07 20:32:38 +0300

    Update mp3

---
+++ lib/src/mp3/mp3_view.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'mp3_controller.dart';
+import '../../routes/app_pages.dart';
+
+class Mp3View extends StatelessWidget {
+  const Mp3View({
+    super.key,
+  });
+
+  final double paddingValue = 8.0;
+
+  @override
+  Widget build(context) {
+    final Mp3Controller c = Get.find();
+
+    final double adjustedWidth = Get.width - 2 * 8;
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Obx(() => Text("${c.task.value?.title}")),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.query_stats),
+              color: Theme.of(context).colorScheme.primary,
+              onPressed: () {
+                Get.toNamed(Routes.DEBUG);
+              },
+            ),
+          ]),
+      body: Center(
+        child: IconButton.outlined(
+          style: const ButtonStyle(
+            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+          ),
+          constraints: BoxConstraints(
+            maxWidth: adjustedWidth,
+            minWidth: adjustedWidth,
+            maxHeight: adjustedWidth,
+            minHeight: adjustedWidth,
+          ),
+          iconSize: adjustedWidth,
+          icon: const Icon(Icons.blur_on),
+          onPressed: () {},
+        ), // IconButton
+      ), // Center
+    );
+  }
+}
