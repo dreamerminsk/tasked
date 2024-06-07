2161aaec99a1d87e77dc7974087025b8aa59ce59
Micah Teeuws <tutmirage@gmail.com>
2024-06-07 15:47:31 +0300

    Update app_routes.dart

--- lib/src/routes/app_routes.dart
+++ lib/src/routes/app_routes.dart
@@ -5,7 +5,6 @@ abstract class Routes {
 
   static const HOME = _Paths.HOME;
   static const DETAILS = _Paths.DETAILS;
-  static const WATCHLIST = _Paths.WATCHLIST;
   static const TASKLIST = _Paths.TASKLIST;
   static const TASK = _Paths.TASK;
   static const SNOOKEREVENTS = _Paths.SNOOKEREVENTS;
@@ -19,14 +18,12 @@ abstract class Routes {
   static const JSON = _Paths.JSON;
   static const THEME = _Paths.THEME;
   static const FILES = _Paths.FILES;
-  static const RANDOM = _Paths.RANDOM;
   static const SCANNER = _Paths.SCANNER;
 }
 
 abstract class _Paths {
   static const HOME = '/home';
   static const DETAILS = '/details';
-  static const WATCHLIST = '/watchlist';
   static const TASKLIST = '/tasklist';
   static const TASK = '/task';
   static const SNOOKEREVENTS = '/task/events';
@@ -40,6 +37,5 @@ abstract class _Paths {
   static const JSON = '/json';
   static const THEME = '/theme';
   static const FILES = '/files';
-  static const RANDOM = '/watchlist/random';
   static const SCANNER = '/scanner';
 }
