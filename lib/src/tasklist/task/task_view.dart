6575e0e3bb5836b4a800b04c8f48c5f6c181889e
Micah Teeuws <tutmirage@gmail.com>
2024-06-09 23:20:58 +0300

    Update task_view.dart

--- lib/src/tasklist/task/task_view.dart
+++ lib/src/tasklist/task/task_view.dart
@@ -4,6 +4,7 @@ import 'package:get/get.dart';
 //import 'task_controller.dart';
 import '../../routes/app_pages.dart';
 import 'task_item.dart';
+import '../../core/widgets/icon_buttons.dart';
 
 class TaskView extends StatelessWidget {
   const TaskView({
@@ -16,13 +17,9 @@ class TaskView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text("${task.title}"), actions: <Widget>[
-        IconButton(
-          icon: Icon(Icons.query_stats),
-          color: Theme.of(context).colorScheme.primary,
-          onPressed: () {
-            Get.toNamed(Routes.DEBUG);
-          },
-        ),
+        DebugIconButton(
+route: Routes.DEBUG,
+), // DebugIconButton
       ]),
       body: Center(
         child: Icon(Icons.blur_on,
