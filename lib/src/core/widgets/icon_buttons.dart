028fa8bbd97e5c20422d070ad6828bb8b117b9d1
Micah Teeuws <tutmirage@gmail.com>
2024-06-09 23:04:13 +0300

    Update widgets

---
+++ lib/src/core/widgets/icon_buttons.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+enum IconButtonVariant { standard, filled, filledTonal, outlined }
+
+class DebugIconButton extends StatelessWidget {
+  const DebugIconButton({
+    super.key,
+    this.color,
+    required this.route,
+  }):_variant = IconButtonVariant.standard;
+
+const DebugIconButton.filled({
+    super.key,
+    this.color,
+    required this.route,
+  }):_variant = IconButtonVariant.filled;
+
+const DebugIconButton.filledTonal({
+    super.key,
+    this.color,
+    required this.route,
+  }):_variant = IconButtonVariant.filledTonal;
+
+const DebugIconButton.outlined({
+    super.key,
+    this.color,
+    required this.route,
+  }):_variant = IconButtonVariant.outlined;
+
+  final Color? color;
+  final String route;
+  final  IconButtonVariant _variant;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return switch (variant) {
+      IconButtonVariant.filled      => IconButton.filled(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+      IconButtonVariant.filledTonal => IconButton.filledTonal(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+      IconButtonVariant.outlined    => IconButton.outlined(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+      IconButtonVariant.standard    => IconButton(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+    };
+  }
+}
