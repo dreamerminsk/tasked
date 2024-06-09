e691273dda42fd2b82a1921f68b3e358609e79f1
Micah Teeuws <tutmirage@gmail.com>
2024-06-09 22:40:35 +0300

    Update debug_icon_button.dart

--- lib/src/core/widgets/debug_icon_button.dart
+++ lib/src/core/widgets/debug_icon_button.dart
@@ -1,1 +1,27 @@
-class DebugIconButton extends StatelessWidget {}
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+
+class DebugIconButton extends StatelessWidget {
+  const DebugIconButton({
+    super.key,
+    this.color,
+   required this.route,
+  });
+
+  final Color? color;
+final String route;
+
+  @override
+  Widget build(BuildContext context) {
+final colorScheme = Theme.of(context).colorScheme;
+
+    return IconButton(
+          icon: const Icon(Icons.query_stats),
+          color: color ?? colorScheme.primary,
+          onPressed: () {
+            Get.toNamed(route);
+          }, // onPressed
+        ), // IconButton
+  }
+}
