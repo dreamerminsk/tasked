bcbd856f5be10e8fc416c5c90f0df4b1653e4e93
dreamerminsk <tutmirage@gmail.com>
2024-06-09 19:44:26 +0000

    dart format

--- lib/src/core/widgets/debug_icon_button.dart
+++ lib/src/core/widgets/debug_icon_button.dart
@@ -1,27 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-
 class DebugIconButton extends StatelessWidget {
   const DebugIconButton({
     super.key,
     this.color,
-   required this.route,
+    required this.route,
   });
 
   final Color? color;
-final String route;
+  final String route;
 
   @override
   Widget build(BuildContext context) {
-final colorScheme = Theme.of(context).colorScheme;
+    final colorScheme = Theme.of(context).colorScheme;
 
     return IconButton(
-          icon: const Icon(Icons.query_stats),
-          color: color ?? colorScheme.primary,
-          onPressed: () {
-            Get.toNamed(route);
-          }, // onPressed
-        ); // IconButton
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ); // IconButton
   }
 }
