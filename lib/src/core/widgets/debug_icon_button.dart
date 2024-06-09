76e2d85bd13c55c78080c09eaecc866b0b6644dd
Micah Teeuws <tutmirage@gmail.com>
2024-06-09 22:56:21 +0300

    Update debug_icon_button.dart

--- lib/src/core/widgets/debug_icon_button.dart
+++ lib/src/core/widgets/debug_icon_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+enum IconButtonVariant { standard, filled, filledTonal, outlined }
+
 class DebugIconButton extends StatelessWidget {
   const DebugIconButton({
     super.key,
@@ -15,12 +17,35 @@ class DebugIconButton extends StatelessWidget {
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-    return IconButton(
+    return switch (variant) {
+      IconButtonVariant.filled      => IconButton.filled(
       icon: const Icon(Icons.query_stats),
       color: color ?? colorScheme.primary,
       onPressed: () {
         Get.toNamed(route);
       }, // onPressed
-    ); // IconButton
+    ), // IconButton
+      IconButtonVariant.filledTonal => IconButton.filledTonal(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+      IconButtonVariant.outlined    => IconButton.outlined(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+      IconButtonVariant.standard    => IconButton(
+      icon: const Icon(Icons.query_stats),
+      color: color ?? colorScheme.primary,
+      onPressed: () {
+        Get.toNamed(route);
+      }, // onPressed
+    ), // IconButton
+    };
   }
 }
