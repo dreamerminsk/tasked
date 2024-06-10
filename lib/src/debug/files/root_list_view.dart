d389a2934f44748b2fa26cfb079c667a32219ded
dreamerminsk <tutmirage@gmail.com>
2024-06-10 18:35:31 +0000

    dart format

--- lib/src/debug/files/root_list_view.dart
+++ lib/src/debug/files/root_list_view.dart
@@ -5,77 +5,77 @@ import 'package:path_provider/path_provider.dart';
 import 'root_card.dart';
 
 class RootListView extends StatelessWidget {
-
   final roots = RxSet<String>();
 
   @override
   Widget build(context) {
     final colorScheme = Theme.of(context).colorScheme;
 
-if (roots.isEmpty) {
-getTemporaryDirectory().then((d) {
+    if (roots.isEmpty) {
+      getTemporaryDirectory().then((d) {
         roots.add(d.path);
       });
 
-getApplicationSupportDirectory().then((d) {
-        roots.add(d.path);
-        roots.add(d.parent.path);
-      });
-
-getLibraryDirectory().then((d) {
-        roots.add(d.path);
-      });
-
-getApplicationDocumentsDirectory().then((d) {
+      getApplicationSupportDirectory().then((d) {
         roots.add(d.path);
         roots.add(d.parent.path);
       });
 
-getApplicationCacheDirectory().then((d) {
+      getLibraryDirectory().then((d) {
+        roots.add(d.path);
+      });
+
+      getApplicationDocumentsDirectory().then((d) {
         roots.add(d.path);
         roots.add(d.parent.path);
       });
 
-getExternalStorageDirectory().then((d) {
+      getApplicationCacheDirectory().then((d) {
+        roots.add(d.path);
+        roots.add(d.parent.path);
+      });
+
+      getExternalStorageDirectory().then((d) {
         roots.add(d?.path);
         roots.add(d?.parent.path);
       });
 
-getExternalCacheDirectories().then((ds) {
-if (ds!= null) {
-roots.addAll(ds);
-}
+      getExternalCacheDirectories().then((ds) {
+        if (ds != null) {
+          roots.addAll(ds);
+        }
       });
 
-getExternalStorageDirectories().then((ds) {
-if (ds!= null) {
-roots.addAll(ds);
-}
+      getExternalStorageDirectories().then((ds) {
+        if (ds != null) {
+          roots.addAll(ds);
+        }
       });
 
-getDownloadsDirectory().then((d) {
+      getDownloadsDirectory().then((d) {
         roots.add(d?.path);
       });
-}
+    }
 
     return Scaffold(
       appBar: AppBar(title: const Text('ROOTS'), actions: <Widget>[]),
 
-body: ObxValue(
-              (data) => ListView.separated(
-    padding: const EdgeInsets.all(8),
-    itemCount: data.length,
-    itemBuilder: (BuildContext context, int index) {
-      return RootCard(
-                title: data[index],
-                background: colorScheme.primary,
-                foreground: colorScheme.onPrimary,
-              ); // RootCard
-    },
-    separatorBuilder: (BuildContext context, int index) => const Divider(),
-  ), // ListView.separated
-              roots,
-            ), // ObxValue
+      body: ObxValue(
+        (data) => ListView.separated(
+          padding: const EdgeInsets.all(8),
+          itemCount: data.length,
+          itemBuilder: (BuildContext context, int index) {
+            return RootCard(
+              title: data[index],
+              background: colorScheme.primary,
+              foreground: colorScheme.onPrimary,
+            ); // RootCard
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+        ), // ListView.separated
+        roots,
+      ), // ObxValue
     );
   }
 }
