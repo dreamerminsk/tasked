b4dc8f03206c9bed09cd0e2a0e36de9652d7c6e9
Micah Teeuws <tutmirage@gmail.com>
2024-06-10 21:13:55 +0300

    Update root_list_view.dart

--- lib/src/debug/files/root_list_view.dart
+++ lib/src/debug/files/root_list_view.dart
@@ -5,19 +5,51 @@ import 'package:path_provider/path_provider.dart';
 import 'root_card.dart';
 
 class RootListView extends StatelessWidget {
-  final app = ''.obs;
-
-  final temp = ''.obs;
-
-  final docs = ''.obs;
-
-  final cache = ''.obs;
 
   final roots = RxSet<String>();
 
   @override
   Widget build(context) {
     final colorScheme = Theme.of(context).colorScheme;
+
+if (roots.isEmpty) {
+getTemporaryDirectory().then((d) {
+        roots.add(d.path);
+        roots.add(d.parent.path);
+      });
+
+getApplicationSupportDirectory().then((d) {
+        roots.add(d.path);
+        roots.add(d.parent.path);
+      });
+
+getLibraryDirectory().then((d) {
+        roots.add(d.path);
+        roots.add(d.parent.path);
+      });
+
+getApplicationDocumentsDirectory().then((d) {
+        roots.add(d.path);
+        roots.add(d.parent.path);
+      });
+
+getApplicationCacheDirectory().then((d) {
+        roots.add(d.path);
+        roots.add(d.parent.path);
+      });
+
+
+
+
+     
+   
+
+      final Directory? result = await getExternalStorageDirectory();
+      final List<Directory>? result = await getExternalCacheDirectories();
+      final List<Directory>? result = await getExternalStorageDirectories();
+      final Directory? result = await getDownloadsDirectory();
+
+}
 
     if (temp.value.isEmpty) {
       getTemporaryDirectory().then((d) => temp.value = d.path);
