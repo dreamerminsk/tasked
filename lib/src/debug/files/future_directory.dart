6a0e75b88227cac46a8acfdbb6715a2bbc5bb126
dreamerminsk <tutmirage@gmail.com>
2024-06-15 11:42:47 +0000

    dart format

--- lib/src/debug/files/future_directory.dart
+++ lib/src/debug/files/future_directory.dart
@@ -1,38 +1,38 @@
 class FutureDirectory extends StatelessWidget {
   FutureDirectory({
-super.key,
+    super.key,
     required this._request,
   });
 
-Future<Directory?> _request;
+  Future<Directory?> _request;
 
- @override
+  @override
   Widget build(BuildContext context) {
-    return FutureBuilder(future: getData(),
-builder: (ctx, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(
-                    '${snapshot.error} occurred',
-                    style: TextStyle(fontSize: 18),
-                  ),
-               );
-              } else if (snapshot.hasData) {
-                final data = snapshot.data as Directory;
-                return Center(
-                  child: Text(
-                    '$data',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                );
-              }
-            }
+    return FutureBuilder(
+      future: getData(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
             return Center(
-              child: CircularProgressIndicator(),
+              child: Text(
+                '${snapshot.error} occurred',
+                style: TextStyle(fontSize: 18),
+              ),
             );
-          },
-);
-}
-
+          } else if (snapshot.hasData) {
+            final data = snapshot.data as Directory;
+            return Center(
+              child: Text(
+                '$data',
+                style: TextStyle(fontSize: 18),
+              ),
+            );
+          }
+        }
+        return Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
+  }
 }
