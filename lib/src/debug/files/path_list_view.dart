e55d99dd158f1ca3f56f61db664018a607e0272c
Micah Teeuws <tutmirage@gmail.com>
2024-06-22 21:13:20 +0300

    Update path_list_view.dart

--- lib/src/debug/files/path_list_view.dart
+++ lib/src/debug/files/path_list_view.dart
@@ -21,9 +21,17 @@ class PathListView extends StatelessWidget {
           Padding(
             padding: EdgeInsets.fromLTRB(20, 8, 8, 8),
             child: RequestedPath(
-              name: 'TemporaryDirectory',
+              name: 'ApplicationCacheDirectory',
               request: Future.delayed(
-                  const Duration(seconds: 1), getTemporaryDirectory()),
+                  const Duration(seconds: 1), getApplicationCacheDirectory()),
+            ), // RequestedPath
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.fromLTRB(8, 8, 20, 8),
+            child: RequestedPath(
+              name: 'ApplicationDocumentsDirectory',
+              request: Future.delayed(const Duration(seconds: 2),
+                  getApplicationDocumentsDirectory()),
             ), // RequestedPath
           ), // Padding
           Padding(
@@ -31,39 +39,15 @@ class PathListView extends StatelessWidget {
             child: RequestedPath(
               name: 'ApplicationSupportDirectory',
               request: Future.delayed(
-                  const Duration(seconds: 2), getApplicationSupportDirectory()),
+                  const Duration(seconds: 3), getApplicationSupportDirectory()),
             ), // RequestedPath
           ), // Padding
           Padding(
             padding: EdgeInsets.fromLTRB(20, 8, 8, 8),
             child: RequestedPath(
-              name: 'LibraryDirectory',
+              name: 'DownloadsDirectory',
               request: Future.delayed(
-                  const Duration(seconds: 3), getLibraryDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 20, 8),
-            child: RequestedPath(
-              name: 'ApplicationDocumentsDirectory',
-              request: Future.delayed(const Duration(seconds: 4),
-                  getApplicationDocumentsDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(20, 8, 8, 8),
-            child: RequestedPath(
-              name: 'ApplicationCacheDirectory',
-              request: Future.delayed(
-                  const Duration(seconds: 5), getApplicationCacheDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 20, 8),
-            child: RequestedPath(
-              name: 'ExternalStorageDirectory',
-              request: Future.delayed(
-                  const Duration(seconds: 6), getExternalStorageDirectory()),
+                  const Duration(seconds: 4), getDownloadsDirectory()),
             ), // RequestedPath
           ), // Padding
           Padding(
@@ -71,7 +55,7 @@ class PathListView extends StatelessWidget {
             child: RequestedPaths(
               name: 'ExternalCacheDirectories',
               request: Future.delayed(
-                  const Duration(seconds: 7), getExternalCacheDirectories()),
+                  const Duration(seconds: 5), getExternalCacheDirectories()),
             ), // RequestedPaths
           ), // Padding
           Padding(
@@ -79,17 +63,33 @@ class PathListView extends StatelessWidget {
             child: RequestedPaths(
               name: 'ExternalStorageDirectories',
               request: Future.delayed(
-                  const Duration(seconds: 8), getExternalStorageDirectories()),
+                  const Duration(seconds: 6), getExternalStorageDirectories()),
             ), // RequestedPaths
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.fromLTRB(8, 8, 20, 8),
+            child: RequestedPath(
+              name: 'ExternalStorageDirectory',
+              request: Future.delayed(
+                  const Duration(seconds: 7), getExternalStorageDirectory()),
+            ), // RequestedPath
           ), // Padding
           Padding(
             padding: EdgeInsets.fromLTRB(20, 8, 8, 8),
             child: RequestedPath(
-              name: 'DownloadsDirectory',
+              name: 'LibraryDirectory',
               request: Future.delayed(
-                  const Duration(seconds: 9), getDownloadsDirectory()),
+                  const Duration(seconds: 8), getLibraryDirectory()),
             ), // RequestedPath
           ), // Padding
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 8, 8, 8),
+            child: RequestedPath(
+              name: 'TemporaryDirectory',
+              request: Future.delayed(
+                  const Duration(seconds: 9), getTemporaryDirectory()),
+            ), // RequestedPath
+          ), // Padding       
         ],
       ), // ListView
     );
