fb46cd87041081dccc80d0c9c20f6971f82a6f57
Micah Teeuws <tutmirage@gmail.com>
2024-06-22 11:59:44 +0300

    Update requested_path.dart

--- lib/src/debug/files/requested_path.dart
+++ lib/src/debug/files/requested_path.dart
@@ -1,14 +1,17 @@
 class RequestedPath extends StatelessWidget {
   RequestedPath({
     super.key,
+    required this._name,
     required this._request,
   });
 
-  Future<Directory?> _request;
+  final _name;
+  final Future<Directory?> _request;
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
+     final textTheme = Theme.of(context).textTheme;
 
     return FutureBuilder(
       future: _request,
@@ -21,21 +24,28 @@ class RequestedPath extends StatelessWidget {
             color = colorScheme.error;
             onColor = colorScheme.onError;
           } else if (snapshot.hasData) {
-            color = colorScheme.primary;
+            data = snapshot.data as Directory?;
+            if (data == null) {
+              color = colorScheme.primaryContainer;
+            onColor = colorScheme.onPrimaryContainer;
+            } else {
+              color = colorScheme.primary;
             onColor = colorScheme.onPrimary;
-            data = snapshot.data as Directory;
+            }
           }
         }
-        return Container(
-          width: cardWidth,
-          height: cardHeight,
+
+        return AnimatedContainer(
+          width: Get.width-2*8.0,
+          height: (Get.width-2*8.0) / 1.618 / 2.0,
+          duration: Duration(seconds: 4.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                'path_provider: $cardWidth x $cardHeight',
+                '$_name',
                 style: textTheme.headlineSmall!
-                    .copyWith(color: colorScheme.onPrimary),
+                    .copyWith(color: onColor),
               ), // Text
             ],
           ), // Column
@@ -44,7 +54,7 @@ class RequestedPath extends StatelessWidget {
             borderRadius: const BorderRadius.all(
               Radius.circular(20),
             ), // BorderRadius
-            color: colorScheme.primary,
+            color: color,
           ), // BoxDecoration
         ); //Container
       },
