12d26c5e80d72559c1772c37d56ad3388440388b
Micah Teeuws <tutmirage@gmail.com>
2024-06-22 12:41:16 +0300

    Update requested_path.dart

--- lib/src/debug/files/requested_path.dart
+++ lib/src/debug/files/requested_path.dart
@@ -1,15 +1,17 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class RequestedPath extends StatelessWidget {
   RequestedPath({
     super.key,
-    required this._name,
-    required this._request,
+    required this.name,
+    required this.request,
   });
 
-  final String _name;
-  final Future<Directory?> _request;
+  final String name;
+  final Future<Directory?> request;
 
   @override
   Widget build(BuildContext context) {
@@ -17,7 +19,7 @@ class RequestedPath extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
 
     return FutureBuilder(
-      future: _request,
+      future: request,
       builder: (ctx, snapshot) {
         color = colorScheme.surfaceVariant;
         onColor = colorScheme.onSurfaceVariant;
@@ -46,7 +48,7 @@ class RequestedPath extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text(
-                '$_name',
+                '$name',
                 style: textTheme.headlineSmall!.copyWith(color: onColor),
               ), // Text
             ],
