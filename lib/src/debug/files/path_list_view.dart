33aad4fbf771aa033ccf01f141da7158e8d3aafc
Micah Teeuws <tutmirage@gmail.com>
2024-06-23 22:56:38 +0300

    Update path_list_view.dart

--- lib/src/debug/files/path_list_view.dart
+++ lib/src/debug/files/path_list_view.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -8,90 +7,72 @@ import 'requested_paths.dart';
 
 class PathListView extends StatelessWidget {
   @override
-  Widget build(context) {
-    //final colorScheme = Theme.of(context).colorScheme;
+  Widget build(BuildContext context) {
+    final List<Map<String, dynamic>> paths = [
+      {
+        'name': 'ApplicationCacheDirectory',
+        'request': Future.delayed(const Duration(seconds: 1), () => getApplicationCacheDirectory()),
+        'isList': false
+      },
+      {
+        'name': 'ApplicationDocumentsDirectory',
+        'request': Future.delayed(const Duration(seconds: 2), () => getApplicationDocumentsDirectory()),
+        'isList': false
+      },
+      {
+        'name': 'ApplicationSupportDirectory',
+        'request': Future.delayed(const Duration(seconds: 3), () => getApplicationSupportDirectory()),
+        'isList': false
+      },
+      {
+        'name': 'DownloadsDirectory',
+        'request': Future.delayed(const Duration(seconds: 4), () => getDownloadsDirectory()),
+        'isList': false
+      },
+      {
+        'name': 'ExternalCacheDirectories',
+        'request': Future.delayed(const Duration(seconds: 5), () => getExternalCacheDirectories()),
+        'isList': true
+      },
+      {
+        'name': 'ExternalStorageDirectories',
+        'request': Future.delayed(const Duration(seconds: 6), () => getExternalStorageDirectories()),
+        'isList': true
+      },
+      {
+        'name': 'ExternalStorageDirectory',
+        'request': Future.delayed(const Duration(seconds: 7), () => getExternalStorageDirectory()),
+        'isList': false
+      },
+      {
+        'name': 'LibraryDirectory',
+        'request': Future.delayed(const Duration(seconds: 8), () => getLibraryDirectory()),
+        'isList': false
+      },
+      {
+        'name': 'TemporaryDirectory',
+        'request': Future.delayed(const Duration(seconds: 9), () => getTemporaryDirectory()),
+        'isList': false
+      },
+    ];
 
     return Scaffold(
-      appBar:
-          AppBar(title: const Text('path_provider 2.1.3'), actions: <Widget>[]),
+      appBar: AppBar(title: const Text('path_provider 2.1.3')),
+      body: ListView.builder(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        itemCount: paths.length,
+        itemBuilder: (context, index) {
+          final path = paths[index];
+          final padding = EdgeInsets.fromLTRB(index.isEven ? 24 : 8, 8, index.isEven ? 8 : 24, 8);
 
-      body: ListView(
-        padding: const EdgeInsets.fromLTRB(0, 8, 0, 8),
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(24, 8, 8, 8),
-            child: RequestedPath(
-              name: 'ApplicationCacheDirectory',
-              request: Future.delayed(const Duration(seconds: 1),
-                  () => getApplicationCacheDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 24, 8),
-            child: RequestedPath(
-              name: 'ApplicationDocumentsDirectory',
-              request: Future.delayed(const Duration(seconds: 2),
-                  () => getApplicationDocumentsDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(24, 8, 8, 8),
-            child: RequestedPath(
-              name: 'ApplicationSupportDirectory',
-              request: Future.delayed(const Duration(seconds: 3),
-                  () => getApplicationSupportDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 24, 8),
-            child: RequestedPath(
-              name: 'DownloadsDirectory',
-              request: Future.delayed(
-                  const Duration(seconds: 4), () => getDownloadsDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(24, 8, 8, 8),
-            child: RequestedPaths(
-              name: 'ExternalCacheDirectories',
-              request: Future.delayed(const Duration(seconds: 5),
-                  () => getExternalCacheDirectories()),
-            ), // RequestedPaths
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 24, 8),
-            child: RequestedPaths(
-              name: 'ExternalStorageDirectories',
-              request: Future.delayed(const Duration(seconds: 6),
-                  () => getExternalStorageDirectories()),
-            ), // RequestedPaths
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(24, 8, 8, 8),
-            child: RequestedPath(
-              name: 'ExternalStorageDirectory',
-              request: Future.delayed(const Duration(seconds: 7),
-                  () => getExternalStorageDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(8, 8, 24, 8),
-            child: RequestedPath(
-              name: 'LibraryDirectory',
-              request: Future.delayed(
-                  const Duration(seconds: 8), () => getLibraryDirectory()),
-            ), // RequestedPath
-          ), // Padding
-          Padding(
-            padding: EdgeInsets.fromLTRB(24, 8, 8, 8),
-            child: RequestedPath(
-              name: 'TemporaryDirectory',
-              request: Future.delayed(
-                  const Duration(seconds: 9), () => getTemporaryDirectory()),
-            ), // RequestedPath
-          ), // Padding
-        ],
-      ), // ListView
+          return Padding(
+            padding: padding,
+            child: path['isList']
+                ? RequestedPaths(name: path['name'], request: path['request'])
+                : RequestedPath(name: path['name'], request: path['request']),
+          );
+        },
+      ),
     );
   }
 }
