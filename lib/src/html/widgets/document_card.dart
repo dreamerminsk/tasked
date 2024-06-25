66a6ebd9c770ba91a004a43b9f8d0d0efb438d93
Micah Teeuws <tutmirage@gmail.com>
2024-06-25 23:23:22 +0300

    Update document_card.dart

--- lib/src/html/widgets/document_card.dart
+++ lib/src/html/widgets/document_card.dart
@@ -4,24 +4,33 @@ import 'package:get/get.dart';
 class DocumentCard extends StatelessWidget {
   const DocumentCard({
     super.key,
+    required this.location,
   });
+
+final String location;
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    return GestureDetector(
+    final uri = Uri.tryParse(location);
+
+    return InkWell(
       onTap: () {},
-      child: Container(
+      borderRadius: const BorderRadius.all(
+            Radius.circular(20),
+          ), // BorderRadius
+      child: AspectRatio(
+aspectRatio: 2*1.618,
+child: Container(
         width: Get.width - 2 * 8.0,
-        height: (Get.width - 2 * 8.0) / 2.0 / 1.618,
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'html ::: document',
+              '${uri?.host}',
               style: textTheme.headlineLarge!.copyWith(
                 color: colorScheme.onPrimary,
                 fontWeight: FontWeight.w300,
@@ -29,7 +38,6 @@ class DocumentCard extends StatelessWidget {
             ), // Text
           ],
         ), // Column
-        //margin: const EdgeInsets.all(16),
         decoration: BoxDecoration(
           borderRadius: const BorderRadius.all(
             Radius.circular(20),
@@ -37,6 +45,7 @@ class DocumentCard extends StatelessWidget {
           color: colorScheme.primary,
         ), // BoxDecoration
       ), // Container
-    ); // GestureDetector
+), // AspectRatio
+    ); // InkWell
   }
 }
