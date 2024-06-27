27962f1175baa9136c05ee4ed3898cc34fdf4400
Micah Teeuws <tutmirage@gmail.com>
2024-06-27 15:33:49 +0300

    Update theme

---
+++ lib/src/debug/theme/sample_font_weight.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class SampleFontWeight extends StatelessWidget {
+  final String title;
+  final Object? object;
+
+  const SampleFontWeight({
+    super.key,
+    required this.title,
+    required this.object,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Material(
+      elevation: 4,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        width: double.infinity,
+        height: Get.width / 2 / 1.618, // Golden ratio height
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: colorScheme.primary,
+        ),
+        padding: const EdgeInsets.all(16),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: Center(
+                child: Text(
+                  _objectToString(object),
+                  style: textTheme.titleLarge!.copyWith(
+                    fontWeight: FontWeight.w700,
+                    color: colorScheme.onPrimary,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+            const SizedBox(height: 8),
+            Divider(
+              color: colorScheme.onPrimary,
+              thickness: 1.5,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              title,
+              style: textTheme.titleLarge!.copyWith(
+                fontSize: textTheme.titleLarge!.fontSize! - 1,
+                color: colorScheme.onPrimary,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _objectToString(Object? object) {
+    if (object == null) {
+      return "null";
+    }
+    return object.toString();
+  }
+}
