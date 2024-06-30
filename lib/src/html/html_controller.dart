a7b38b336593bb060a258a1ab22a5b8baecf1afb
Micah Teeuws <tutmirage@gmail.com>
2024-06-30 23:33:18 +0300

    Update html_controller.dart

--- lib/src/html/html_controller.dart
+++ lib/src/html/html_controller.dart
@@ -4,6 +4,7 @@ import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:nanoid2/nanoid2.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 import '../debug/debug_controller.dart';
 import '../tasklist/task/task_item.dart';
