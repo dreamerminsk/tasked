096696ae15a389284f1e45f5fa78a841dd510864
Micah Teeuws <tutmirage@gmail.com>
2024-07-05 09:02:52 +0300

    Update mp3_file

---
+++ lib/src/mp3/mp3_file/mp3_file_view.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'mp3_controller.dart';
+import '../routes/app_pages.dart';
+import '../core/widgets/icon_buttons.dart';
+
+class Mp3FileView extends StatelessWidget {
+  const Mp3FileView({
+    super.key,
+  });
+
+  final double paddingValue = 8.0;
+
+  @override
+  Widget build(context) {
+    final Mp3FileController c = Get.find();
+
+    //const double padding = 8.0;
+    //final double adjustedWidth = Get.width - 2 * padding;
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('Mp3FileView'),
+          actions: <Widget>[
+            DebugIconButton(
+              route: Routes.DEBUG,
+            ), // DebugIconButton
+          ]),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Container(
+            width: Get.width,
+            height: Get.width / 1.618 / 2,
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: colorScheme.primaryFixed,
+              border: Border(
+                top: BorderSide(
+                  color: colorScheme.primary,
+                  width: 2.0,
+                ), // BorderSide
+                bottom: BorderSide(
+                  color: colorScheme.primary,
+                  width: 2.0,
+                ), // BorderSide
+              ), // Border
+            ), // BoxDecoration
+            child: Obx(() => Text(c.mp3Files.length.toString(),
+                style: textTheme.bodyLarge!.copyWith(
+                  color: colorScheme.onPrimaryFixed,
+                ))),
+          ), // Container
+          Obx(
+            () {
+              if (c.mp3Files.isEmpty) {
+                return Expanded(
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Center(child: Text('No MP3 files found.')),
+                  ),
+                );
+              } else {
+                return Expanded(
+                  child: MediaQuery.removePadding(
+                    context: context,
+                    removeTop: true,
+                    child: ListView.builder(
+                      itemCount: c.mp3Files.length,
+                      itemBuilder: (context, index) {
+                        final file = c.mp3Files[index];
+                        return ListTile(
+                          title: Text(
+                            file.path.split('/').last,
+                          ),
+                          subtitle: Text(file.parent.path),
+                        );
+                      },
+                    ),
+                  ),
+                );
+              }
+            },
+          ), // Obx
+        ], // children
+      ), // Column
+      floatingActionButton: FloatingActionButton.extended(
+        icon: const Icon(Icons.audiotrack_outlined),
+        label: Text('Add'),
+        onPressed: c.pickFile,
+      ),
+    );
+  }
+}
