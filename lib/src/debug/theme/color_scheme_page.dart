9393f69c94d28800367aacf79026f80b54df4025
Micah Teeuws <tutmirage@gmail.com>
2024-07-06 23:29:48 +0300

    Update color_scheme_page.dart

--- lib/src/debug/theme/color_scheme_page.dart
+++ lib/src/debug/theme/color_scheme_page.dart
@@ -224,6 +224,14 @@ class ColorSchemePage extends StatelessWidget {
           Padding(
             padding: EdgeInsets.all(8),
             child: SampleColor(
+              title: 'surfaceContainerHighest',
+              background: colorScheme.surfaceContainerHighest,
+              foreground: colorScheme.onSurface,
+            ), // ColorContainer
+          ), // Padding
+          Padding(
+            padding: EdgeInsets.all(8),
+            child: SampleColor(
               title: 'inverseSurface',
               background: colorScheme.inverseSurface,
               foreground: colorScheme.onInverseSurface,
