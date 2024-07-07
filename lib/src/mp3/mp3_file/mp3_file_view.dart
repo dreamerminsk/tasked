d0d86fdc083db383bc0c0998c838bb867ddfff2d
Micah Teeuws <tutmirage@gmail.com>
2024-07-08 00:14:08 +0300

    Update mp3_file_view.dart

--- lib/src/mp3/mp3_file/mp3_file_view.dart
+++ lib/src/mp3/mp3_file/mp3_file_view.dart
@@ -32,67 +32,8 @@ class Mp3FileView extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: <Widget>[
-          Container(
-            width: Get.width,
-            height: Get.width / 1.618 / 2,
-            padding: EdgeInsets.all(16.0),
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: colorScheme.primaryFixed,
-              border: Border(
-                top: BorderSide(
-                  color: colorScheme.primary,
-                  width: 2.0,
-                ), // BorderSide
-                bottom: BorderSide(
-                  color: colorScheme.primary,
-                  width: 2.0,
-                ), // BorderSide
-              ), // Border
-            ), // BoxDecoration
-            child: Obx(() => Text(c.mp3Files.length.toString(),
-                style: textTheme.bodyLarge!.copyWith(
-                  color: colorScheme.onPrimaryFixed,
-                ))),
-          ), // Container
-          Obx(
-            () {
-              if (c.mp3Files.isEmpty) {
-                return Expanded(
-                  child: Align(
-                    alignment: Alignment.center,
-                    child: Center(child: Text('No MP3 files found.')),
-                  ),
-                );
-              } else {
-                return Expanded(
-                  child: MediaQuery.removePadding(
-                    context: context,
-                    removeTop: true,
-                    child: ListView.builder(
-                      itemCount: c.mp3Files.length,
-                      itemBuilder: (context, index) {
-                        final file = c.mp3Files[index];
-                        return ListTile(
-                          title: Text(
-                            file.path.split('/').last,
-                          ),
-                          subtitle: Text(file.parent.path),
-                        );
-                      },
-                    ),
-                  ),
-                );
-              }
-            },
-          ), // Obx
         ], // children
       ), // Column
-      floatingActionButton: FloatingActionButton.extended(
-        icon: const Icon(Icons.audiotrack_outlined),
-        label: Text('Add'),
-        onPressed: c.pickFile,
-      ),
     );
   }
 }
