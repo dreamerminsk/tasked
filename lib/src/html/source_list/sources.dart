ccae2543c030294090ee10bbd347829a7a672066
Micah Teeuws <tutmirage@gmail.com>
2024-07-07 14:24:30 +0300

    Update sources.dart

--- lib/src/html/source_list/sources.dart
+++ lib/src/html/source_list/sources.dart
@@ -8,16 +8,28 @@ class WebSource implements Source {
   WebSource({required this.location});
 
   final String location;
+
+  String toString() {
+return location;
+}
 }
 
 class LocalSource implements Source {
   LocalSource({required this.location});
 
   final String location;
+
+String toString() {
+return location;
+}
 }
 
 class AssetSource implements Source {
   AssetSource({required this.location});
 
   final String location;
+
+String toString() {
+return location;
+}
 }
