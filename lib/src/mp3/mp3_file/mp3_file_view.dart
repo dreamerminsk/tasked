538dd4ff4dcdad73ccb38c4bc29ed869eabd3a24
Micah Teeuws <tutmirage@gmail.com>
2024-07-08 00:35:02 +0300

    Update mp3_file_view.dart

--- lib/src/mp3/mp3_file/mp3_file_view.dart
+++ lib/src/mp3/mp3_file/mp3_file_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import 'mp3_file_controller.dart';
+//import 'mp3_file_controller.dart';
 import '../../routes/app_pages.dart';
 import '../../core/widgets/icon_buttons.dart';
 
@@ -14,12 +14,12 @@ class Mp3FileView extends StatelessWidget {
 
   @override
   Widget build(context) {
-    final Mp3FileController c = Get.find();
+    //final Mp3FileController c = Get.find();
 
     //const double padding = 8.0;
     //final double adjustedWidth = Get.width - 2 * padding;
-    final textTheme = Theme.of(context).textTheme;
-    final colorScheme = Theme.of(context).colorScheme;
+    //final textTheme = Theme.of(context).textTheme;
+    //final colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
       appBar: AppBar(title: Text('Mp3FileView'), actions: <Widget>[
