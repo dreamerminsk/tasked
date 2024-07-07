e68dc9a1984b4ee5874441cc3b38120e10ef6706
Micah Teeuws <tutmirage@gmail.com>
2024-07-07 23:31:09 +0300

    Update source_item.dart

--- lib/src/html/source_list/widgets/source_item.dart
+++ lib/src/html/source_list/widgets/source_item.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../sources.dart';
+
 class SourceItem extends StatelessWidget {
   const SourceItem({
     super.key,
