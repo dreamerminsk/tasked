01905b000317b01821da02a5285387fd8e09f7b7
Micah Teeuws <tutmirage@gmail.com>
2024-07-07 14:22:37 +0300

    Update sources.dart

--- lib/src/html/source_list/sources.dart
+++ lib/src/html/source_list/sources.dart
@@ -1,27 +1,23 @@
 abstract class Source {
-
-factory Source.web(String location) => WebSource(location: location);
-
-factory Source.local(String location) => LicalSource(location: location);
-
-factory Source.asset(String location) => AssetSource(location: location);
-
+  factory Source.web(String location) => WebSource(location: location);
+  factory Source.local(String location) => LocalSource(location: location);
+  factory Source.asset(String location) => AssetSource(location: location);
 }
 
 class WebSource implements Source {
-WebSource({required this.location});
+  WebSource({required this.location});
 
-final String location;
+  final String location;
 }
 
 class LocalSource implements Source {
-LocalSource({required this.location});
+  LocalSource({required this.location});
 
-final String location;
+  final String location;
 }
 
 class AssetSource implements Source {
-AssetSource({required this.location});
+  AssetSource({required this.location});
 
-final String location;
+  final String location;
 }
