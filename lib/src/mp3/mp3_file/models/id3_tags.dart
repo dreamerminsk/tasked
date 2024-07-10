72474235388b45be4782e08f0dc96982eedea9ea
Micah Teeuws <tutmirage@gmail.com>
2024-07-10 22:08:01 +0300

    Update mp3_file

---
+++ lib/src/mp3/mp3_file/models/id3_tags.dart
@@ -0,0 +1,11 @@
+abstract class Id3Tag{}
+
+class Id3v1Tag extends Id3Tag {}
+
+abstract class Id3v2Tag extends Id3Tag {}
+
+class Id3v2_2Tag extends Id3v2Tag {}
+
+class Id3v2_3Tag extends Id3v2Tag {}
+
+class Id3v2_4Tag extends Id3v2Tag {}
