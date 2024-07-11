57bdf131e89002a88d09c7245c189b528d908314
Micah Teeuws <tutmirage@gmail.com>
2024-07-11 23:52:28 +0300

    Update mp3_file_view.dart

--- lib/src/mp3/mp3_file/mp3_file_view.dart
+++ lib/src/mp3/mp3_file/mp3_file_view.dart
@@ -23,7 +23,7 @@ class Mp3FileView extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-          title: Text('Mp3FileView: ${mp3file.value.path.split("/").last}'),
+          title: Text('Mp3FileView: ${c.mp3file.value.path.split("/").last}'),
           actions: <Widget>[
             DebugIconButton(
               route: Routes.DEBUG,
