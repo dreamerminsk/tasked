bdd48c73b05da5663a9d3f95d1b5a91cb83ee3ca
Micah Teeuws <tutmirage@gmail.com>
2024-07-11 23:29:46 +0300

    Update mp3_file_view.dart

--- lib/src/mp3/mp3_file/mp3_file_view.dart
+++ lib/src/mp3/mp3_file/mp3_file_view.dart
@@ -32,10 +32,13 @@ class Mp3FileView extends StatelessWidget {
         children: <Widget>[
           Obx(()=>
 c.header == null 
-?SizedBox(height: 0,)
+? Card(
+title:Text(c.mp3file.value?.path.split('/').last),
+subtitle:Text('pos: 0, size: ${c.mp3file.value?.lengthSync()}') ,
+)
 : Card(
 title: Text(c.header.runtimeType.toString()),
-subtitle:Text('pos: 0, size: ${10+c.header.size}') ,
+subtitle:Text('pos: 0, size: ${10+c.header.value?.size}') ,
 ) // Card
 ), // Obx
 ], // children
