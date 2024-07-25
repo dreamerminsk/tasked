82ab835900927c9a93283b1bcdd14805f6461a76
Micah Teeuws <tutmirage@gmail.com>
2024-07-25 15:22:33 +0300

    Update sample_color_green.dart

--- lib/src/debug/theme/sample_color_green.dart
+++ lib/src/debug/theme/sample_color_green.dart
@@ -4,12 +4,10 @@ import 'package:get/get.dart';
 import '../../core/color_utils.dart';
 
 class SampleColorGreen extends StatelessWidget {
-  final String title;
   final int green;
 
   const SampleColorGreen({
     Key? key,
-    required this.title,
     required this.green,
   }) : super(key: key);
 
