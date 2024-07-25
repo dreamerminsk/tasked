38d94bb0171de59f76af26a841d281f8b361fa4b
Micah Teeuws <tutmirage@gmail.com>
2024-07-25 15:16:42 +0300

    Update sample_color_red.dart

--- lib/src/debug/theme/sample_color_red.dart
+++ lib/src/debug/theme/sample_color_red.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,60 +8,72 @@ class SampleColorRed extends StatelessWidget {
   final int red;
 
   const SampleColorRed({
-    super.key,
+    Key? key,
     required this.title,
     required this.red,
-  });
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
-    final child = Container(
-      width: double.infinity,
-      height: Get.width / 2 / 1.618,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Color.fromARGB(255, red, 0, 0),
-      ), // BoxDecoration
-      padding: EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          AspectRatio(
-            aspectRatio: 1.0,
-            child: Container(
-              alignment: Alignment.center,
-              child: Text('R',
-                  style: Theme.of(context)
-                      .textTheme
-                      .displayLarge!
-                      .copyWith(color: colorScheme.onPrimary)),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(12),
-                color: colorScheme.primary,
-              ), // BoxDecoration
-            ), // Container
-          ), // AspectRatio
-          Container(
-            alignment: Alignment.center,
-            child: Text('$alpha',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineSmall!
-                    .copyWith(color:ColorUtils.contrastThemeColor(Color.fromARGB(255, red, 0, 0)))),
-            color: Colors.transparent,
-          ), // Container
-        ],
-      ), // Row
-    ); // Container
+    final containerHeight = Get.width / 2 / 1.618; 
+    final containerColor = Color.fromARGB(255, red, 0, 0);
 
     return Material(
       elevation: 4,
       borderRadius: BorderRadius.circular(20),
-      child: child,
-    ); // Material
+      child: Container(
+        width: double.infinity,
+        height: containerHeight,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          color: containerColor,
+        ),
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            _buildRedTextBox(context, colorScheme),
+            _buildAlphaTextBox(context, containerColor),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRedTextBox(BuildContext context, ColorScheme colorScheme) {
+    return AspectRatio(
+      aspectRatio: 1.0,
+      child: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: colorScheme.primary,
+        ),
+        child: Text(
+          'R',
+          style: Theme.of(context)
+              .textTheme
+              .displayLarge!
+              .copyWith(color: colorScheme.onPrimary),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAlphaTextBox(BuildContext context, Color containerColor) {
+    return Container(
+      alignment: Alignment.center,
+      color: Colors.transparent,
+      child: Text(
+        '$red',
+        style: Theme.of(context)
+            .textTheme
+            .headlineSmall!
+            .copyWith(color: ColorUtils.contrastThemeColor(containerColor)),
+      ),
+    );
   }
 }
