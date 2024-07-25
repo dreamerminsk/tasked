44a50660d4dca9a2c41aaee58f044be94f9c197b
Micah Teeuws <tutmirage@gmail.com>
2024-07-25 16:10:01 +0300

    Update sample_color_blue.dart

--- lib/src/debug/theme/sample_color_blue.dart
+++ lib/src/debug/theme/sample_color_blue.dart
@@ -13,7 +13,7 @@ class SampleColorBlue extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
+    //final textTheme = Theme.of(context).textTheme;
     final colorScheme = Theme.of(context).colorScheme;
 
     final containerHeight = Get.width / 2 / 1.618; 
