25eec9fe5963a9033c3e726d79d44c87c0b7a17d
Micah Teeuws <tutmirage@gmail.com>
2024-07-27 00:07:42 +0300

    Update sample_color_alpha.dart

--- lib/src/debug/theme/sample_color_alpha.dart
+++ lib/src/debug/theme/sample_color_alpha.dart
@@ -46,7 +46,7 @@ class SampleColorAlpha extends StatelessWidget {
             child: Text('$alpha',
                 style: Theme.of(context)
                     .textTheme
-                    .displayLarge!
+                    .headlineLarge!
                     .copyWith(color: colorScheme.onPrimaryContainer)),
             color: Colors.transparent,
           ), // Container
