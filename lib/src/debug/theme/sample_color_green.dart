69b20f67c00ba9903618aee0a52b98e815993ae1
Micah Teeuws <tutmirage@gmail.com>
2024-07-27 00:10:13 +0300

    Update sample_color_green.dart

--- lib/src/debug/theme/sample_color_green.dart
+++ lib/src/debug/theme/sample_color_green.dart
@@ -66,10 +66,10 @@ class SampleColorGreen extends StatelessWidget {
       alignment: Alignment.center,
       color: Colors.transparent,
       child: Text(
-        '$green',
+        '#${green.toRadixString(16).padLeft(2,'0').toUpperCase()}',
         style: Theme.of(context)
             .textTheme
-            .displayLarge!
+            .headlineLarge!
             .copyWith(color: ColorUtils.contrastThemeColor(containerColor)),
       ),
     );
