638bb303e3493f7a01ba74ce57b082381c94d747
Micah Teeuws <tutmirage@gmail.com>
2024-07-28 23:00:34 +0300

    Update sample_font_color.dart

--- lib/src/debug/theme/sample_font_color.dart
+++ lib/src/debug/theme/sample_font_color.dart
@@ -107,6 +107,10 @@ final ValueChanged<NamedColor>? onChanged;
 
 class ColorNotifier extends ChangeNotifier{
 
+ColorNotifier() {
+_initColors();
+}
+
 final _colors = <NamedColor>[];
 
 int _colorIdx = 0;
@@ -114,12 +118,20 @@ int _colorIdx = 0;
 Color get color => _colors[_colorIdx];
 
 void next() {
+if (_colors.isEmpty) return;
 _colorIdx = (_colorIdx + 1) % _colors.length;
 notifyListeners();
 }
 
 void prev() {
+if (_colors.isEmpty) return;
 _colorIdx = (_colorIdx - 1 + _colors.length) % _colors.length;
 notifyListeners();
+}
+
+void _initColors() {
+_colors.addAll(NamedColors.primaries.map((item)=>item.shade500).toList());
+_colors.add(NamedColors.black);
+_colors.add(NamedColors.white);
 }
 }
