d19a557d04cadf1b267f6f84537ad99494193f14
Micah Teeuws <tutmirage@gmail.com>
2024-09-16 20:34:06 +0300

    Update mp3_file

---
+++ lib/src/audio/mp3_file/mp3_file_controller.dart
@@ -0,0 +1,53 @@
+import 'dart:core';
+import 'dart:io';
+
+import 'package:get/get.dart';
+import 'package:nanoid2/nanoid2.dart';
+
+import '../../debug/debug_controller.dart';
+
+import 'models/id3_tags.dart';
+
+class Mp3FileController extends GetxController {
+  final id = nanoid();
+
+  final started = DateTime.now();
+
+  final DebugController debug = Get.find(tag: 'debugger');
+
+  final mp3file = Rxn<File>();
+
+  RandomAccessFile? _raf;
+
+  final header = Rxn<Id3v2TagHeader>();
+
+  @override
+  void onInit() {
+    super.onInit();
+    debug.logInit(this.runtimeType.toString(), id, started);
+  }
+
+  @override
+  void onReady() async {
+    super.onReady();
+    mp3file.value = Get.arguments;
+    _raf = await mp3file.value?.open();
+    final headerBuf = List<int>.filled(10, 0);
+    final headerBufLength = await _raf?.readInto(headerBuf) ?? 0;
+    if (headerBufLength > 9) {
+      String identifier = String.fromCharCodes(headerBuf.sublist(0, 3));
+      if (identifier == 'ID3') {
+        header.value = Id3v2TagHeader.parse(headerBuf);
+      }
+    }
+  }
+
+  @override
+  void onClose() {
+    debug.logClose(this.runtimeType.toString(), id, DateTime.now());
+    if (_raf != null) {
+      _raf?.close();
+    }
+    super.onClose();
+  }
+}
