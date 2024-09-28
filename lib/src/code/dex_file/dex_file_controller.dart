e08e6f75f7d7f95a7eaa4b632c20fe551011539a
Micah Teeuws <tutmirage@gmail.com>
2024-09-28 11:49:35 +0300

    Update dex_file_controller.dart

--- lib/src/code/dex_file/dex_file_controller.dart
+++ lib/src/code/dex_file/dex_file_controller.dart
@@ -15,11 +15,11 @@ class DexFileController extends GetxController {
 
   final DebugController debug = Get.find(tag: 'debugger');
 
-  final dexfile = Rxn<File>();
+  final dexFile = Rxn<File>();
 
   RandomAccessFile? _raf;
 
-  final header = Rxn<DexHeader>();
+ 
 
   @override
   void onInit() {
@@ -30,16 +30,8 @@ class DexFileController extends GetxController {
   @override
   void onReady() async {
     super.onReady();
-    mp3file.value = Get.arguments;
+    dexFile.value = Get.arguments;
     _raf = await dexfile.value?.open();
-    final headerBuf = List<int>.filled(10, 0);
-    final headerBufLength = await _raf?.readInto(headerBuf) ?? 0;
-    if (headerBufLength > 9) {
-      String identifier = String.fromCharCodes(headerBuf.sublist(0, 3));
-      if (identifier == 'ID3') {
-        header.value = DexHeader.parse(headerBuf);
-      }
-    }
   }
 
   @override
