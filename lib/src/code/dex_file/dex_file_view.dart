a8d67be8f8573d4d9eda55b4f709599515bfe2b3
Micah Teeuws <tutmirage@gmail.com>
2024-09-28 12:07:57 +0300

    Update dex_file_view.dart

--- lib/src/code/dex_file/dex_file_view.dart
+++ lib/src/code/dex_file/dex_file_view.dart
@@ -82,6 +82,13 @@ Card(
                 isThreeLine: true,
              ), // ListTile
           ), // Card
+          Card(
+             child: ListTile(
+                title: Text('CALL_SITE_IDS'),
+                subtitle: Text('pos: 0, size: 0'),
+                isThreeLine: true,
+             ), // ListTile
+          ), // Card
         ], // children
       ), // Column
     );
