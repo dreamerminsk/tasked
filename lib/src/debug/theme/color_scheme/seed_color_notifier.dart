0b28f9d4f6eb3893904dc4d9debcf26334560adc
Micah Teeuws <tutmirage@gmail.com>
2024-10-01 15:36:18 +0300

    Update seed_color_notifier.dart

--- lib/src/debug/theme/color_scheme/seed_color_notifier.dart
+++ lib/src/debug/theme/color_scheme/seed_color_notifier.dart
@@ -3,4 +3,8 @@ class SeedColorNotifier extends ChangeNotifier{
 _colorIdx = 0;
 _shadeIdx = 0;
 
+
+      NamedMaterialColor get seedColor = NamedColors.primaries[_colorIdx];
+      NamedColor get shadeColor = NamedColors.primaries[_colorIdx].shades[_shadeIdx];
+
 }
