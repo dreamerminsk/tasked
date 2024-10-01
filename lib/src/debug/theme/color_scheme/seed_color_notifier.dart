e00b018685d10c07f4525eddc35e0b3b7c02d310
Micah Teeuws <tutmirage@gmail.com>
2024-10-01 21:38:23 +0300

    Update seed_color_notifier.dart

--- lib/src/debug/theme/color_scheme/seed_color_notifier.dart
+++ lib/src/debug/theme/color_scheme/seed_color_notifier.dart
@@ -7,4 +7,12 @@ _shadeIdx = 0;
       NamedMaterialColor get seedColor => NamedColors.primaries[_colorIdx];
       NamedColor get shadeColor => NamedColors.primaries[_colorIdx].shades[_shadeIdx];
 
+void nextColor() {
+
+}
+
+void prevColor() {
+
+}
+
 }
