0a9886dd44274534cd2dec40a25c06a39fe94fc8
dreamerminsk <tutmirage@gmail.com>
2024-10-01 19:00:57 +0000

    dart format

--- lib/src/debug/theme/color_scheme/seed_color_notifier.dart
+++ lib/src/debug/theme/color_scheme/seed_color_notifier.dart
@@ -1,43 +1,37 @@
 import 'package:flutter/material.dart';
 
-
 import '../../../core/colors.dart';
 import '../../../core/color_utils.dart';
 
+class SeedColorNotifier extends ChangeNotifier {
+  int _colorIdx = 0;
+  int _shadeIdx = 0;
 
+  NamedMaterialColor get seedColor => NamedColors.primaries[_colorIdx];
+  NamedColor get shadeColor =>
+      NamedColors.primaries[_colorIdx].shades[_shadeIdx];
 
-class SeedColorNotifier extends ChangeNotifier{
+  void nextColor() {
+    _colorIdx = (_colorIdx + 1) % NamedColors.primaries.length;
+    _shadeIdx = _shadeIdx % seedColor.shades.length;
+    notifyListeners();
+  }
 
-int _colorIdx = 0;
-int _shadeIdx = 0;
+  void prevColor() {
+    const colors = NamedColors.primaries.length;
+    _colorIdx = (_colorIdx + colors - 1) % colors;
+    _shadeIdx = _shadeIdx % seedColor.shades.length;
+    notifyListeners();
+  }
 
+  void nextShade() {
+    _shadeIdx = (_shadeIdx + 1) % seedColor.shades.length;
+    notifyListeners();
+  }
 
-      NamedMaterialColor get seedColor => NamedColors.primaries[_colorIdx];
-      NamedColor get shadeColor => NamedColors.primaries[_colorIdx].shades[_shadeIdx];
-
-void nextColor() {
-_colorIdx = (_colorIdx + 1) % NamedColors.primaries.length;
-_shadeIdx = _shadeIdx % seedColor.shades.length;
-notifyListeners();
-}
-
-void prevColor() {
-const colors = NamedColors.primaries.length;
-_colorIdx = (_colorIdx +colors- 1) % colors;
-_shadeIdx = _shadeIdx % seedColor.shades.length;
-notifyListeners();
-}
-
-
-void nextShade() {
-_shadeIdx = (_shadeIdx + 1) % seedColor.shades.length;
-notifyListeners();
-}
-
-void prevShade() {
-const shades = seedColor.shades.length;
-_colorIdx = (_colorIdx +shades- 1) % shades;
-notifyListeners();
-}
-
+  void prevShade() {
+    const shades = seedColor.shades.length;
+    _colorIdx = (_colorIdx + shades - 1) % shades;
+    notifyListeners();
+  }
 }
