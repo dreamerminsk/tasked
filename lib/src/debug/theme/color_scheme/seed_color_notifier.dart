21ed00d8fe8bfb44b6e5bd7292c6809d0bd9262b
Micah Teeuws <tutmirage@gmail.com>
2024-10-01 21:55:59 +0300

    Update seed_color_notifier.dart

--- lib/src/debug/theme/color_scheme/seed_color_notifier.dart
+++ lib/src/debug/theme/color_scheme/seed_color_notifier.dart
@@ -1,3 +1,8 @@
+import '../../../core/colors.dart';
+import '../../../core/color_utils.dart';
+
+
+
 class SeedColorNotifier extends ChangeNotifier{
 
 _colorIdx = 0;
@@ -8,11 +13,28 @@ _shadeIdx = 0;
       NamedColor get shadeColor => NamedColors.primaries[_colorIdx].shades[_shadeIdx];
 
 void nextColor() {
-
+_colorIdx = (_colorIdx + 1) % NamedColors.primaries.length;
+_shadeIdx = _shadeIdx % seedColor.shades.length;
+notifyListeners();
 }
 
 void prevColor() {
+const colors = NamedColors.primaries.length;
+_colorIdx = (_colorIdx +colors- 1) % colors;
+_shadeIdx = _shadeIdx % seedColor.shades.length;
+notifyListeners();
+}
 
+
+void nextShade() {
+_shadeIdx = (_shadeIdx + 1) % seedColor.shades.length;
+notifyListeners();
+}
+
+void prevShade() {
+const shades = seedColor.shades.length;
+_colorIdx = (_colorIdx +shades- 1) % shades;
+notifyListeners();
 }
 
 }
