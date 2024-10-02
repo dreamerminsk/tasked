baa601c3daaffeba1191dcba272eafc59d370546
Micah Teeuws <tutmirage@gmail.com>
2024-10-02 14:09:53 +0300

    Update seed_color_notifier.dart

--- lib/src/debug/theme/color_scheme/seed_color_notifier.dart
+++ lib/src/debug/theme/color_scheme/seed_color_notifier.dart
@@ -10,11 +10,11 @@ class SeedColorNotifier extends ChangeNotifier {
   NamedMaterialColor get seedColor => NamedColors.primaries[_colorIdx];
   NamedColor get shadeColor => seedColor.shades[_shadeIdx];
 
-NamedMaterialColor get nextSeedColor => NamedColors.primaries[_colorIdx];
-  NamedColor get nextShadeColor => seedColor.shades[_shadeIdx];
+NamedMaterialColor get nextSeedColor => NamedColors.primaries[(_colorIdx + 1) % NamedColors.primaries.length];
+  NamedColor get nextShadeColor => seedColor.shades[(_shadeIdx + 1) % seedColor.shades.length];
 
-NamedMaterialColor get prevSeedColor => NamedColors.primaries[_colorIdx];
-  NamedColor get prevShadeColor => seedColor.shades[_shadeIdx];
+NamedMaterialColor get prevSeedColor => NamedColors.primaries[(_colorIdx - 1 + NamedColors.primaries.length) % NamedColors.primaries.length];
+  NamedColor get prevShadeColor => seedColor.shades[_shadeIdx = (_shadeIdx - 1 + seedColor.shades.length) % seedColor.shades.length];
 
   void nextColor() {
     _colorIdx = (_colorIdx + 1) % NamedColors.primaries.length;
