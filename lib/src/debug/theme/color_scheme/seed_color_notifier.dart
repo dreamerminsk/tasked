7ad7fc3a6c6f37c13aae77017177160ef36daf7f
Micah Teeuws <tutmirage@gmail.com>
2024-10-02 14:12:53 +0300

    Update seed_color_notifier.dart

--- lib/src/debug/theme/color_scheme/seed_color_notifier.dart
+++ lib/src/debug/theme/color_scheme/seed_color_notifier.dart
@@ -10,21 +10,25 @@ class SeedColorNotifier extends ChangeNotifier {
   NamedMaterialColor get seedColor => NamedColors.primaries[_colorIdx];
   NamedColor get shadeColor => seedColor.shades[_shadeIdx];
 
-NamedMaterialColor get nextSeedColor => NamedColors.primaries[(_colorIdx + 1) % NamedColors.primaries.length];
-  NamedColor get nextShadeColor => seedColor.shades[(_shadeIdx + 1) % seedColor.shades.length];
+  NamedMaterialColor get nextSeedColor =>
+      NamedColors.primaries[(_colorIdx + 1) % NamedColors.primaries.length];
+  NamedColor get nextShadeColor =>
+      seedColor.shades[(_shadeIdx + 1) % seedColor.shades.length];
 
-NamedMaterialColor get prevSeedColor => NamedColors.primaries[(_colorIdx - 1 + NamedColors.primaries.length) % NamedColors.primaries.length];
-  NamedColor get prevShadeColor => seedColor.shades[_shadeIdx = (_shadeIdx - 1 + seedColor.shades.length) % seedColor.shades.length];
+  NamedMaterialColor get prevSeedColor =>
+      NamedColors.primaries[(_colorIdx - 1 + NamedColors.primaries.length) % NamedColors.primaries.length];
+  NamedColor get prevShadeColor =>
+      seedColor.shades[(_shadeIdx - 1 + seedColor.shades.length) % seedColor.shades.length];
 
   void nextColor() {
     _colorIdx = (_colorIdx + 1) % NamedColors.primaries.length;
-    _shadeIdx = 0; // Reset to first shade when color changes
+    _shadeIdx = 0; // Reset shade index when color changes
     notifyListeners();
   }
 
   void prevColor() {
     _colorIdx = (_colorIdx - 1 + NamedColors.primaries.length) % NamedColors.primaries.length;
-    _shadeIdx = 0; // Reset to first shade when color changes
+    _shadeIdx = 0; // Reset shade index when color changes
     notifyListeners();
   }
 
