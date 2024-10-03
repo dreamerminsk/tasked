bc61fdcd23602270ac16366109551bf43c82694f
Micah Teeuws <tutmirage@gmail.com>
2024-10-03 09:28:37 +0300

    Update color_scheme

---
+++ lib/src/debug/theme/color_scheme/seed_color_spinner.dart
@@ -0,0 +1,46 @@
+class SeedColorSpinner extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+    //final textTheme = Theme.of(context).textTheme;
+    //final colorScheme = colorSchemeN.value;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(Icons.color_lens),
+          color: Colors.yellow,
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.color_lens),
+          color: Colors.orange.shade300,
+          onPressed: () {},
+        ),
+        AnimatedContainer(
+          duration: const Duration(
+            seconds: 1,
+          ),
+          padding: EdgeInsets.all(8.0),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Text('orange'),
+        ),
+        IconButton(
+          icon: Icon(Icons.color_lens),
+          color: Colors.orange.shade700,
+          onPressed: () {},
+        ),
+        IconButton(
+          icon: Icon(Icons.color_lens),
+          color: Colors.brown,
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+}
