5af825046eb83d5c5ffadc106ae1276b5a107209
Micah Teeuws <tutmirage@gmail.com>
2024-10-11 15:46:54 +0300

    Update scheme_variant_notifier.dart

--- lib/src/debug/theme/color_scheme/scheme_variant_notifier.dart
+++ lib/src/debug/theme/color_scheme/scheme_variant_notifier.dart
@@ -3,22 +3,22 @@ import 'package:flutter/material.dart';
 import '../../../core/colors.dart';
 
 class SchemeVariantNotifier extends ChangeNotifier {
-  int _variantIdx = 1;
-
   int _prevVariantIdx = 0;
+
+  int _variantIdx = 1;
 
   int _nextVariantIdx = 2;
 
   final variantCount = DynamicSchemeVariant.values.length;
+
+  NamedMaterialColor get prevColor => NamedColors.primaries[_prevColorIdx];
+  NamedColor get prevShade => color.shades[_prevShadeIdx];
 
   NamedMaterialColor get color => NamedColors.primaries[_colorIdx];
   NamedColor get shade => color.shades[_shadeIdx];
 
   NamedMaterialColor get nextColor => NamedColors.primaries[_nextColorIdx];
   NamedColor get nextShade => color.shades[_nextShadeIdx];
-
-  NamedMaterialColor get prevColor => NamedColors.primaries[_prevColorIdx];
-  NamedColor get prevShade => color.shades[_prevShadeIdx];
 
   void incVariantIdx() {
     _prevVariantIdx = _variantIdx;
