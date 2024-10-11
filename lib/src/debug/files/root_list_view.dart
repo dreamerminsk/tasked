43c6cae02294586e705a46571e058e51bd019fed
Micah Teeuws <tutmirage@gmail.com>
2024-10-11 19:56:22 +0300

    Update root_list_view.dart

--- lib/src/debug/files/root_list_view.dart
+++ lib/src/debug/files/root_list_view.dart
@@ -167,6 +167,16 @@ getExternalStorageDirectories(StorageDirectory.dcim).then((ds) {
           snackPosition: SnackPosition.BOTTOM);
     });
 
+getExternalStorageDirectories(StorageDirectory.documents).then((ds) {
+      if (ds != null) {
+        roots.value.addAll(ds.map<String>((item) => item.path));
+        roots.refresh();
+      }
+    }).catchError((e) {
+      Get.snackbar('getExternalStorageDirectories(StorageDirectory.documents)', '$e',
+          snackPosition: SnackPosition.BOTTOM);
+    });
+
     getDownloadsDirectory().then((d) {
       if (d != null) {
         roots.value.add(d.path);
