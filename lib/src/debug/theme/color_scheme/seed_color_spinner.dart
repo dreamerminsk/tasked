57416dadd1b62e020353e68c3d6b288f77612677
Micah Teeuws <tutmirage@gmail.com>
2024-10-11 15:22:39 +0300

    Update seed_color_spinner.dart

--- lib/src/debug/theme/color_scheme/seed_color_spinner.dart
+++ lib/src/debug/theme/color_scheme/seed_color_spinner.dart
@@ -18,6 +18,7 @@ class SeedColorSpinner extends StatelessWidget {
       children: <Widget>[
         Consumer<SeedColorNotifier>(
           builder: (context, value, child) => IconButton(
+            iconSize: 32,
             icon: Icon(Icons.color_lens),
             color: value.prevColor.colorSwatch,
             onPressed: () {
@@ -27,6 +28,7 @@ class SeedColorSpinner extends StatelessWidget {
         ), // Consumer
         Consumer<SeedColorNotifier>(
           builder: (context, value, child) => IconButton(
+            iconSize: 28,
             icon: Icon(Icons.color_lens),
             color: value.prevShade.color,
             onPressed: () {
@@ -60,6 +62,7 @@ class SeedColorSpinner extends StatelessWidget {
         ), // Consumer
         Consumer<SeedColorNotifier>(
           builder: (context, value, child) => IconButton(
+            iconSize: 28,
             icon: Icon(Icons.color_lens),
             color: value.nextShade.color,
             onPressed: () {
@@ -69,6 +72,7 @@ class SeedColorSpinner extends StatelessWidget {
         ), // Consumer
         Consumer<SeedColorNotifier>(
           builder: (context, value, child) => IconButton(
+            iconSize: 32,
             icon: Icon(Icons.color_lens),
             color: value.nextColor.colorSwatch,
             onPressed: () {
