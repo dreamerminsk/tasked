8925d605b37809fee7ba0fe9baf5af8a2560d3f6
Micah Teeuws <tutmirage@gmail.com>
2024-10-12 17:29:58 +0300

    Update root_list_view.dart

--- lib/src/debug/files/root_list_view.dart
+++ lib/src/debug/files/root_list_view.dart
@@ -9,7 +9,7 @@ import 'root_card.dart';
 class RootListView extends StatelessWidget {
   final roots = Rx<SplayTreeSet<String>>(SplayTreeSet<String>());
 
-  Future<void> handleStorageError(String operation, dynamic error) async {
+  Future<Null> handleStorageError(String operation, dynamic error) async {
   Get.snackbar(
     operation,
     '$error',
