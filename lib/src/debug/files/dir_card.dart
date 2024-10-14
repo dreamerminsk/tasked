fa79c4e3a1469b226b0755f217e0c58b738ec5eb
Micah Teeuws <tutmirage@gmail.com>
2024-10-14 09:52:35 +0300

    Update dir_card.dart

--- lib/src/debug/files/dir_card.dart
+++ lib/src/debug/files/dir_card.dart
@@ -22,6 +22,7 @@ class DirCard extends StatelessWidget {
   }
 
   void _fetchFileStat() {
+    FileSystemEntity.type(title).then((value)=>entityType.value=value);
     Directory(title).stat().then((value) => stat.value = value);
   }
 
