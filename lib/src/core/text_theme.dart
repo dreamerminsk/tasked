d043b0952835e7ebfec0bc92ff850fd9572d1f5a
Micah Teeuws <tutmirage@gmail.com>
2024-10-18 21:36:41 +0300

    Update core

---
+++ lib/src/core/text_theme.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart' hide ColorProperty;
+
+import 'color_utils.dart';
+import 'properties.dart';
+
+class NamedTextTheme {
+  const NamedTextTheme({required this.textTheme});
+
+  final TextTheme textTheme;
+
+  factory NamedTextTheme.of(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return NamedTextTheme(textTheme: textTheme);
+  }
+
+  List<ColorProperty> get values => <ColorProperty>[
+      ];
+
+  List<(ColorProperty, ColorProperty)> get pairs =>
+      <(ColorProperty, ColorProperty)>[
+      ];
+}
