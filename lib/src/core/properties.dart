1c3832604915a2619f466744a3a9cc6a75882186
Micah Teeuws <tutmirage@gmail.com>
2024-10-27 14:26:26 +0300

    Update properties.dart

--- lib/src/core/properties.dart
+++ lib/src/core/properties.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart' hide ColorProperty;
 
-class NamedProperty<T> {
-  NamedProperty(this.name, this.value);
+class NamedValue<T> {
+  NamedValue(this.name, this.value);
 
   final String name;
   final T? value;
 }
 
-class ColorProperty extends NamedProperty<Color> {
+class ColorProperty extends NamedValue<Color> {
   ColorProperty(super.name, super.value);
 }
 
-class NamedTextStyle extends NamedProrerty<TextStyle> {
+class NamedTextStyle extends NamedValue<TextStyle> {
   NamedTextStyle(super.name, super.value);
 }
