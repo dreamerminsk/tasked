1ad5f2bb5af49d62c3fbf690e073759e1aa2d2d5
Micah Teeuws <tutmirage@gmail.com>
2024-11-17 00:35:15 +0300

    Update items

---
+++ lib/src/code/dex_file/items/proto_id_item.dart
@@ -0,0 +1,15 @@
+class ProtoIdItem {
+
+final int shortyIdx;
+
+final int returnTypeIdx;
+
+final int parametersOff;
+
+const ProtoIdItem({
+this.shortyIdx,
+this.returnTypeIdx,
+this.parametersOff,
+});
+
+}
