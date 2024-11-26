0d106a8d282ed1b88712aba9cb3ec3dfbe45f102
Micah Teeuws <tutmirage@gmail.com>
2024-11-26 12:58:52 +0300

    Update json

---
+++ lib/src/debug/json/error_widget.dart
@@ -0,0 +1,55 @@
+class ErrorWidget extends StatelessWidget {
+  final Object error;
+
+  const ErrorWidget({
+    Key? key,
+    required this.error,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Card(
+        color: Colors.redAccent,
+        elevation: 4.0,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ), // RoundedRectangleBorder
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.error,
+                color: Colors.white,
+                size: 48.0,
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                error.runtimeType.toString(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 16.0),
+              Text(
+                error.toString(),
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ), // Column
+        ), // Padding
+      ), // Card
+    ); // Padding
+  }
+}
