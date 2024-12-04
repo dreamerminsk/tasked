1ec35baafaded4329bea9ae8e973b692cac5933e
Micah Teeuws <tutmirage@gmail.com>
2024-12-04 17:35:59 +0300

    Update json_controller.dart

--- lib/src/debug/json/json_controller.dart
+++ lib/src/debug/json/json_controller.dart
@@ -8,6 +8,7 @@ import 'package:characters/characters.dart';
 import 'package:nanoid2/nanoid2.dart';
 
 import '../debug_controller.dart';
+import 'nodes.dart';
 
 class JsonController extends GetxController {
   final id = nanoid();
@@ -155,98 +156,4 @@ abstract class Tokens {
 
   /// A carriage return character.
   static const String carriageReturn = '\r';
-}
-
-class JsonNode {
-  final int level;
-  final int offset;
-  final int length;
-
-  const JsonNode({
-    required this.level,
-    required this.offset,
-    required this.length,
-  });
-
-  JsonNode copyWith({
-    int? level,
-    int? offset,
-    int? length,
-  }) {
-    return JsonNode(
-      level: level ?? this.level,
-      offset: offset ?? this.offset,
-      length: length ?? this.length,
-    );
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is JsonNode &&
-        other.level == level &&
-        other.offset == offset &&
-        other.length == length;
-  }
-
-  @override
-  int get hashCode => level.hashCode ^ offset.hashCode ^ length.hashCode;
-}
-
-class ArrayNode extends JsonNode {
-  const ArrayNode({
-    required int level,
-    required int offset,
-    required int length,
-  }) : super(level: level, offset: offset, length: length);
-
-  ArrayNode copyWith({
-    int? level,
-    int? offset,
-    int? length,
-  }) {
-    return ArrayNode(
-      level: level ?? this.level,
-      offset: offset ?? this.offset,
-      length: length ?? this.length,
-    );
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is ArrayNode && super == other;
-  }
-
-  @override
-  int get hashCode => super.hashCode;
-}
-
-class ObjectNode extends JsonNode {
-  const ObjectNode({
-    required int level,
-    required int offset,
-    required int length,
-  }) : super(level: level, offset: offset, length: length);
-
-  ObjectNode copyWith({
-    int? level,
-    int? offset,
-    int? length,
-  }) {
-    return ObjectNode(
-      level: level ?? this.level,
-      offset: offset ?? this.offset,
-      length: length ?? this.length,
-    );
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    return other is ObjectNode && super == other;
-  }
-
-  @override
-  int get hashCode => super.hashCode;
 }
