cff984a20c46ee93cea9998f76ff3b4e90066c6e
Micah Teeuws <tutmirage@gmail.com>
2024-12-04 17:33:36 +0300

    Update json

---
+++ lib/src/debug/json/nodes.dart
@@ -0,0 +1,93 @@
+class JsonNode {
+  final int level;
+  final int offset;
+  final int length;
+
+  const JsonNode({
+    required this.level,
+    required this.offset,
+    required this.length,
+  });
+
+  JsonNode copyWith({
+    int? level,
+    int? offset,
+    int? length,
+  }) {
+    return JsonNode(
+      level: level ?? this.level,
+      offset: offset ?? this.offset,
+      length: length ?? this.length,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is JsonNode &&
+        other.level == level &&
+        other.offset == offset &&
+        other.length == length;
+  }
+
+  @override
+  int get hashCode => level.hashCode ^ offset.hashCode ^ length.hashCode;
+}
+
+class ArrayNode extends JsonNode {
+  const ArrayNode({
+    required int level,
+    required int offset,
+    required int length,
+  }) : super(level: level, offset: offset, length: length);
+
+  ArrayNode copyWith({
+    int? level,
+    int? offset,
+    int? length,
+  }) {
+    return ArrayNode(
+      level: level ?? this.level,
+      offset: offset ?? this.offset,
+      length: length ?? this.length,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is ArrayNode && super == other;
+  }
+
+  @override
+  int get hashCode => super.hashCode;
+}
+
+class ObjectNode extends JsonNode {
+  const ObjectNode({
+    required int level,
+    required int offset,
+    required int length,
+  }) : super(level: level, offset: offset, length: length);
+
+  ObjectNode copyWith({
+    int? level,
+    int? offset,
+    int? length,
+  }) {
+    return ObjectNode(
+      level: level ?? this.level,
+      offset: offset ?? this.offset,
+      length: length ?? this.length,
+    );
+  }
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    return other is ObjectNode && super == other;
+  }
+
+  @override
+  int get hashCode => super.hashCode;
+}
