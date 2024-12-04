6e325cbc244bc5408cdd69fb95d2478073d80675
Micah Teeuws <tutmirage@gmail.com>
2024-12-04 17:47:34 +0300

    Update node_card.dart

--- lib/src/debug/json/widgets/node_card.dart
+++ lib/src/debug/json/widgets/node_card.dart
@@ -10,48 +10,32 @@ class NodeCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double paddingLeft = 8.0;
+    const double paddingRight = 8.0;
+    const double paddingBottom = 4.0;
+    const double paddingTop = 4.0;
+
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: EdgeInsets.fromLTRB(node.level + paddingLeft, paddingTop, paddingRight, paddingBottom),
       child: Card(
-        color: Colors.redAccent,
-        elevation: 4.0,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(8.0),
-        ), // RoundedRectangleBorder
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Icon(
-                Icons.error,
-                color: Colors.white,
-                size: 48.0,
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                error.runtimeType.toString(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 16.0),
-              Text(
-                error.toString(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ],
-          ), // Column
-        ), // Padding
-      ), // Card
-    ); // Padding
+        child: ListTile(
+          leading: _getNodeIcon(node),
+          title: Text('${node.runtimeType}'),
+          subtitle: Text(
+            'Level: ${node.level}, Offset: ${node.offset}, Length: ${node.length}',
+          ),
+        ),
+      ),
+    );
+  }
+
+  Icon _getNodeIcon(JsonNode node) {
+    if (node is ObjectNode) {
+      return Icon(Icons.folder_open, color: Colors.blue);
+    } else if (node is ArrayNode) {
+      return Icon(Icons.list, color: Colors.green);
+    } else {
+      return Icon(Icons.help, color: Colors.grey);
+    }
   }
 }
