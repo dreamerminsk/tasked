6b83729b208f93f96aa9d94020c1d164a13ae341
Micah Teeuws <tutmirage@gmail.com>
2024-12-13 23:29:44 +0300

    Update core

---
+++ lib/src/core/theme_notifier.dart
@@ -0,0 +1,10 @@
+class ThemeNotifiee extends ChangeNotifier {
+  ThemeData _themeData;
+  ThemeChanger(this._themeData);
+
+  get getTheme => _themeData;
+  void setTheme(ThemeData theme) {
+    _themeData = theme;
+    notifyListeners();
+  }
+}
