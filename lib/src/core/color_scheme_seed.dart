6c95fd7132f6ded48a3f2375990f3ca9c905e015
Micah Teeuws <tutmirage@gmail.com>
2024-12-15 22:01:17 +0300

    Update color_scheme_seed.dart

--- lib/src/core/color_scheme_seed.dart
+++ lib/src/core/color_scheme_seed.dart
@@ -6,6 +6,7 @@ import 'colors.dart';
 
 class ColorSchemeSeed {
   const ColorSchemeSeed({
+    required this.materialColor,
     required this.seedColor,
     required this.dynamicSchemeVariant,
     required this.contrastLevel,
@@ -19,22 +20,26 @@ class ColorSchemeSeed {
     final dynamicSchemeVariant = DynamicSchemeVariant
         .values[Random().nextInt(DynamicSchemeVariant.values.length)];
     return ColorSchemeSeed(
+      materialColor: materialColor,
       seedColor: seedColor,
       dynamicSchemeVariant: dynamicSchemeVariant,
       contrastLevel: Random().nextDouble() * 2.0 - 1.0,
     );
   }
 
+  final NamedMaterialColor materialColor;
   final NamedColor seedColor;
   final DynamicSchemeVariant dynamicSchemeVariant;
   final double contrastLevel;
 
   ColorSchemeSeed copyWith({
+    NamedMaterialColor? materialColor,
     NamedColor? seedColor,
     DynamicSchemeVariant? dynamicSchemeVariant,
     double? contrastLevel,
   }) {
     return ColorSchemeSeed(
+      materialColor: materialColor ?? this.materialColor,
       seedColor: seedColor ?? this.seedColor,
       dynamicSchemeVariant: dynamicSchemeVariant ?? this.dynamicSchemeVariant,
       contrastLevel: contrastLevel ?? this.contrastLevel,
@@ -48,6 +53,7 @@ class ColorSchemeSeed {
   @override
   int get hashCode => Object.hashAll(
         <Object?>[
+          materialColor,
           seedColor,
           dynamicSchemeVariant,
           contrastLevel,
@@ -63,6 +69,7 @@ class ColorSchemeSeed {
       return false;
     }
     return other is ColorSchemeSeed &&
+        other.materialColor == materialColor &&
         other.seedColor == seedColor &&
         other.dynamicSchemeVariant == dynamicSchemeVariant &&
         other.contrastLevel == contrastLevel;
