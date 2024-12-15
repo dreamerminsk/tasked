b314c08b3552afdd2ae3a0f7532e23491f457d11
Micah Teeuws <tutmirage@gmail.com>
2024-12-15 19:52:24 +0300

    Update icon_buttons.dart

--- lib/src/core/widgets/icon_buttons.dart
+++ lib/src/core/widgets/icon_buttons.dart
@@ -68,3 +68,72 @@ class DebugIconButton extends StatelessWidget {
     };
   }
 }
+
+
+
+
+class RandomThemeButton extends StatelessWidget {
+  const RandomThemeButton({
+    super.key,
+    this.color,
+    required this.route,
+  }) : _variant = IconButtonVariant.standard;
+
+  const RandomThemeButton.filled({
+    super.key,
+    this.color,
+    required this.route,
+  }) : _variant = IconButtonVariant.filled;
+
+  const RandomThemeButton.filledTonal({
+    super.key,
+    this.color,
+    required this.route,
+  }) : _variant = IconButtonVariant.filledTonal;
+
+  const RandomThemeButton.outlined({
+    super.key,
+    this.color,
+    required this.route,
+  }) : _variant = IconButtonVariant.outlined;
+
+  final Color? color;
+  final String route;
+  final IconButtonVariant _variant;
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return switch (_variant) {
+      IconButtonVariant.filled => IconButton.filled(
+          icon: const Icon(Icons.query_stats),
+          color: color ?? colorScheme.primary,
+          onPressed: () {
+            Get.toNamed(route);
+          }, // onPressed
+        ), // IconButton
+      IconButtonVariant.filledTonal => IconButton.filledTonal(
+          icon: const Icon(Icons.query_stats),
+          color: color ?? colorScheme.primary,
+          onPressed: () {
+            Get.toNamed(route);
+          }, // onPressed
+        ), // IconButton
+      IconButtonVariant.outlined => IconButton.outlined(
+          icon: const Icon(Icons.query_stats),
+          color: color ?? colorScheme.primary,
+          onPressed: () {
+            Get.toNamed(route);
+          }, // onPressed
+        ), // IconButton
+      IconButtonVariant.standard => IconButton(
+          icon: const Icon(Icons.query_stats),
+          color: color ?? colorScheme.primary,
+          onPressed: () {
+            Get.toNamed(route);
+          }, // onPressed
+        ), // IconButton
+    };
+  }
+}
