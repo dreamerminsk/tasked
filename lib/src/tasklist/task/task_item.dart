053a36faa1daf4d75a41576d7dc2ecafd01cf8af
Micah Teeuws <tutmirage@gmail.com>
2024-12-24 09:22:41 +0300

    Update task_item.dart

--- lib/src/tasklist/task/task_item.dart
+++ lib/src/tasklist/task/task_item.dart
@@ -4,22 +4,26 @@ class TaskItem {
   final int id;
   final String title;
   final String path;
+  final GestureTapCallback? onTap;
 
   const TaskItem({
     this.id = 0,
     this.title = 'task 0',
     this.path = Routes.TASK,
+    this.onTap,
   });
 
   TaskItem copyWith({
     int? id,
     String? title,
     String? path,
+    GestureTapCallback? onTap,
   }) {
     return TaskItem(
       id: id ?? this.id,
       title: title ?? this.title,
       path: path ?? this.path,
+      onTap: onTap ?? this.onTap,
     );
   }
 
